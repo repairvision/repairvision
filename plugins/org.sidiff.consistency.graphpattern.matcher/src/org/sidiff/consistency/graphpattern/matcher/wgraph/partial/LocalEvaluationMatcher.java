76abf97c2cfd5d3946a6c95d6d01d9842edf9377
mohrndorf <manuel.ohrndorf@gmx.net>
2016-07-23 13:38:51 +0000

    remove wrong optimization
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2137 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.graphpattern.matcher/src/org/sidiff/consistency/graphpattern/matcher/wgraph/partial/LocalEvaluationMatcher.java
+++ plugins/org.sidiff.consistency.graphpattern.matcher/src/org/sidiff/consistency/graphpattern/matcher/wgraph/partial/LocalEvaluationMatcher.java
@@ -4,7 +4,6 @@ import static org.sidiff.consistency.graphpattern.matcher.tools.MatchingHelper.g
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -44,7 +43,6 @@ public class LocalEvaluationMatcher extends VisitorImpl  {
 	
 	protected IPathRestriction pathRestriction;
 	
-	protected Set<EdgePattern> matched = new HashSet<>();
 	
 	public LocalEvaluationMatcher(
 			MatchingHelper matchingHelper, IConstraintTester constraintTester,  
@@ -78,23 +76,11 @@ public class LocalEvaluationMatcher extends VisitorImpl  {
 		// TODO: BFS with propagation of new matches and upper path length limit!?
 		
 		// Match every path of the local evaluation:
-		// (Should be more efficient than the pairwise paths between variable nodes!)
 		getLocalEvaluation().forEachRemaining(this::matchPath);
 	}
 	
 	private DFSOutgoingPathIterator getLocalEvaluation() {
 		return new DFSOutgoingPathIterator(startVariableNode) {
-			
-			//// Terminate if local evaluation graph == matched graph ////
-			
-			@Override
-			public boolean hasNext() {
-				
-				// Only edges of the local evaluation can be matched:
-				assert ((localEvaluation.size() == matched.size()) == (localEvaluation.equals(matched)));
-
-				return super.hasNext() && (localEvaluation.size() != matched.size());
-			}
 			
 			//// Stay in the local evaluation graph ////
 			
@@ -123,14 +109,9 @@ public class LocalEvaluationMatcher extends VisitorImpl  {
 	private void matchPath(DFSPath path) {
 		if (path.isNewPathEmpty()) return;
 		
-		// Optimization: Lookahead to next edge matching: 
-		//   matched -> match only new matches -> store new matches
-		//   not matched -> match all -> do not store new matches
-		// (Filter already matched path segments -> path approximation)
-		boolean isActualSegmentMatched = false; // Initialization dont't care
-		boolean isNextSegmentMatched = matched.contains(path.getNewPathSegment(0));
-		
 		Collection<EObject> newMatches = new ArrayList<>();
+		NavigableDataStore initialDS = getDataStore(path.getNewPathSegment(0).getSource());
+		initialDS.getMatchIterator().forEachRemaining(newMatches::add);
 		
 		// Walk through the path:
 		for (int i = 0; i < path.getNewPathSize(); i++) {
@@ -142,75 +123,26 @@ public class LocalEvaluationMatcher extends VisitorImpl  {
 			NodePattern sourceNode = pathSegment.getSource();
 			NavigableDataStore sourceDS = getDataStore(sourceNode.getEvaluation());
 			
-			isActualSegmentMatched = isNextSegmentMatched;
-			isNextSegmentMatched = (path.getNewPathSize() > i + 1) 
-					? matched.contains(path.getNewPathSegment(i + 1)) : false;
-			
-			// Process all or only new matches?
-			if (!isActualSegmentMatched) {
-				
-				//// New complete matching ////
-				
-				// Constraint by lookahead optimization...
-				// (Would need newMatches.clear(); otherwise)
-				assert (newMatches.isEmpty());
+			// Process all new matches:
+			if (!newMatches.isEmpty()) {
+				Collection<EObject> targetMatches = new ArrayList<>();
 
-				// Match the given path segment:
-				boolean isEmptyPath = true;
-				
-				for (Iterator<EObject> itSource = sourceDS.getMatchIterator(); itSource.hasNext();) {
-					EObject sourceMatch = itSource.next();
-					
+				for (EObject sourceMatch : newMatches) {
 					for (Iterator<? extends EObject> itTarget = matchEdge(sourceMatch, pathSegment); itTarget.hasNext();) {
 						EObject targetMatch = itTarget.next();
-						boolean isNewTargetMatch = !targetDS.containsMatch(targetMatch);
-						
+
+						// NOTE: We need add the match to the node even if
+						// it is already present, due to the edge matchings.
 						if (matchNode(pathSegment, 
 								sourceNode, sourceDS, sourceMatch, 
 								targetNode, targetDS, targetMatch)) {
-							
-							isEmptyPath = false;
 
-							// Store new matches (if next path segment was already visited):
-							if (isNextSegmentMatched && isNewTargetMatch) {
-								newMatches.add(targetMatch);
-							}
-						}	
-					}
-				}
-		
-				// Stop on empty path:
-				if (isEmptyPath) {
-					break;
-				}
-			} else {
-				
-				//// Propagate (only) new matches through the full path ////
-				
-				if (!newMatches.isEmpty()) {
-					Collection<EObject> targetMatches = new ArrayList<>();
-					
-					for (EObject sourceMatch : newMatches) {
-						for (Iterator<? extends EObject> itTarget = matchEdge(sourceMatch, pathSegment); itTarget.hasNext();) {
-							EObject targetMatch = itTarget.next();
-
-							if (matchNode(pathSegment, 
-									sourceNode, sourceDS, sourceMatch, 
-									targetNode, targetDS, targetMatch)) {
-
-								// Store new matches (if next path segment was already visited):
-								if (isNextSegmentMatched && targetDS.containsMatch(targetMatch)) {
-									targetMatches.add(targetMatch);
-								}
-							}
+							targetMatches.add(targetMatch);
 						}
 					}
-					newMatches = targetMatches;
 				}
+				newMatches = targetMatches;
 			}
-			
-			// Mark edge as visited:
-			matched.add(pathSegment);
 		}
 	}
 	
