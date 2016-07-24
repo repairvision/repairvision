711850c66beae6e45821273d16bc14fd8c7ec4ac
mohrndorf <manuel.ohrndorf@gmx.net>
2016-07-24 20:44:00 +0000

    set graph-pattern resource-type to XMI
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2145 db72bd86-da5d-42d1-ad72-61f8f7c676a0

---
+++ plugins/org.sidiff.consistency.graphpattern/gensrc/org/sidiff/consistency/graphpattern/util/GraphpatternResourceImpl.java
@@ -0,0 +1,33 @@
+/**
+ */
+package org.sidiff.consistency.graphpattern.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.sidiff.consistency.graphpattern.util.GraphpatternResourceFactoryImpl
+ * @generated
+ */
+public class GraphpatternResourceImpl extends XMIResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public GraphpatternResourceImpl(URI uri) {
+		super(uri);
+	}
+	
+	@Override
+	protected boolean useUUIDs() {
+		return true;
+	}
+	
+} //GraphpatternResourceImpl
