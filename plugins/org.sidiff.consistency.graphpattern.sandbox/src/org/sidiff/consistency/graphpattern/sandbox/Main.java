65a1d618060ea97d3ea7fd4aa13169c53e1607b5
mohrndorf <manuel.ohrndorf@gmx.net>
2016-11-13 14:51:57 +0000

    refactoring
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2429 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/Main.java
+++ plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/Main.java
@@ -18,6 +18,8 @@ public class Main {
 	private static String base = "D:\\Workspace\\SiLift\\org.sidiff.consistency.graphpattern.sandbox\\examples\\";
 
 	private static String name = "M004";
+	
+	private static IMatchingEngine engine = new MCCSKrissinelAlgorithm();
 
 	public static void main(String[] args) {
 
@@ -30,7 +32,6 @@ public class Main {
 		// Calculate matching:
 		long start = System.currentTimeMillis();
 
-		IMatchingEngine engine = new MCCSKrissinelAlgorithm();
 		List<Match> matchings = engine.getMatches(subdivisionGraph.getSubdivisionExample());
 
 		System.out.println("Matching Time: " + ((double) (System.currentTimeMillis() - start)) / 1000 + "s");
