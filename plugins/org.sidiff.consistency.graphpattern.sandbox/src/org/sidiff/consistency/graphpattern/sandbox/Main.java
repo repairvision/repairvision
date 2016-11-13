b21afb2eff888d417a8511a0a3860300487dc17b
mohrndorf <manuel.ohrndorf@gmx.net>
2016-11-13 00:26:35 +0000

    alg.
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2426 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/Main.java
+++ plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/Main.java
@@ -2,11 +2,10 @@ package org.sidiff.consistency.graphpattern.sandbox;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.sidiff.consistency.graphpattern.sandbox.algorithms.IMatchingEngine;
-import org.sidiff.consistency.graphpattern.sandbox.algorithms.KrissinelAlgorithm;
+import org.sidiff.consistency.graphpattern.sandbox.algorithms.KrissinelAlgorithm2;
 import org.sidiff.consistency.graphpattern.sandbox.graph.Example;
 import org.sidiff.consistency.graphpattern.sandbox.graph.Match;
 import org.sidiff.consistency.graphpattern.sandbox.graph.Node;
@@ -17,7 +16,7 @@ public class Main {
 
 	private static String base = "D:\\Workspace\\SiLift\\org.sidiff.consistency.graphpattern.sandbox\\examples\\";
 
-	private static String name = "M003";
+	private static String name = "M004";
 
 	public static void main(String[] args) {
 
@@ -27,7 +26,7 @@ public class Main {
 		// Calculate matching:
 		long start = System.currentTimeMillis();
 
-		IMatchingEngine engine = new KrissinelAlgorithm();
+		IMatchingEngine engine = new KrissinelAlgorithm2();
 		List<Match> matchings = engine.getMatches(example);
 
 		System.out.println("Matching Time: " + ((double) (System.currentTimeMillis() - start)) / 1000 + "s");
@@ -39,32 +38,59 @@ public class Main {
 		WriteVisualization.saveVisualization(viz, path);
 
 		// All matches:
-		List<Match> printedMatches = new ArrayList<>(matchings.size());
+		List<Match> postprocessedMatches = new ArrayList<>(matchings.size());
 		
 		for (int i = 0; i < matchings.size(); i++) {
 			Match match = matchings.get(i);
 			
-			if (!containsSubMatch(printedMatches, match)) {
-				printedMatches.add(match);
-				DecimalFormat numbering = new DecimalFormat("000");
-				
-				viz = WriteVisualization.writeVisualization(name, example, matchings.get(i));
-				path = base + name + ".graph_" + numbering.format(i) + ".dot";
-				WriteVisualization.saveVisualization(viz, path);
+			// Remove possible sub-matchings:
+			int subMatch = getSubMatch(postprocessedMatches, match);
+			
+			if (subMatch != -1) {
+				postprocessedMatches.remove(subMatch);
+			}
+			
+			// Add new matching:
+			if (getSuperMatch(postprocessedMatches, match) == -1) {
+				postprocessedMatches.add(match);
 			}
 		}
+		
+		for (int i = 0; i < postprocessedMatches.size(); i++) {
+			Match match = postprocessedMatches.get(i);
+			
+			System.out.println(match.getMatch());
 
-		System.out.println("Visualization (" + printedMatches.size() + "): " + path);
+			viz = WriteVisualization.writeVisualization(name, example, match);
+			DecimalFormat numbering = new DecimalFormat("000");
+			path = base + name + ".graph_" + numbering.format(i) + ".dot";
+			WriteVisualization.saveVisualization(viz, path);
+		}
+
+		System.out.println("Visualization (" + postprocessedMatches.size() + "): " + path);
 
 	}
 	
-	private static boolean containsSubMatch(Collection<Match> superMatchings, Match subMatch) {
-		for (Match superMatch : superMatchings) {
+	private static int getSuperMatch(List<Match> superMatchings, Match subMatch) {
+		for (int i = 0; i < superMatchings.size(); i++) {
+			Match superMatch = superMatchings.get(i);
+			
 			if (isSubmatch(superMatch, subMatch)) {
-				return true;
+				return i;
 			}
 		}
-		return false;
+		return -1;
+	}
+	
+	private static int getSubMatch(List<Match> subMatchings, Match superMatch) {
+		for (int i = 0; i < subMatchings.size(); i++) {
+			Match subMatch = subMatchings.get(i);
+			
+			if (isSubmatch(superMatch, subMatch)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	private static boolean isSubmatch(Match superMatch, Match subMatch) {
--- plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/algorithms/KrissinelAlgorithm2.java
+++ plugins/org.sidiff.consistency.graphpattern.sandbox/src/org/sidiff/consistency/graphpattern/sandbox/algorithms/KrissinelAlgorithm2.java
@@ -9,87 +9,83 @@ import org.sidiff.consistency.graphpattern.sandbox.graph.Example;
 import org.sidiff.consistency.graphpattern.sandbox.graph.Match;
 import org.sidiff.consistency.graphpattern.sandbox.graph.Node;
 
-// TODO: Vismara -> nicht induziert -> Kanten durch Knoten ersetzen
-// RR-Pattern B-Seite Kanten induzieren
-// über Korrespondenzen auf A-Seite propagieren
 public class KrissinelAlgorithm2 implements IMatchingEngine {
 
 	private Example example;
-	
+
 	private List<Match> matchings = new ArrayList<Match>();
-	
+
 	/**
 	 * G1: (partial) Pattern
 	 */
 	private LinkedList<Node> V;
-	
+
 	/**
 	 * G2: Working Graph
 	 */
 	private LinkedList<Node> W;
-	
+
 	/**
-	 * Mapped Pattern 
+	 * Mapped Pattern
 	 */
 	private LinkedList<Node> X = new LinkedList<>();
-	
+
 	/**
 	 * Matching
 	 */
 	private LinkedList<Node> Y = new LinkedList<>();
-	
-	private int nmax = 0;
-	
+
+	// FIXME: Filter sub matchings...
+	private boolean newMatch = false; 
+
 	/**
 	 * Minimal size of common sub-graphs to be found.
 	 */
 	private int n0 = 1;
-	
+
 	private class VMM {
 		// TODO: W.size() => size per slot
 		Node[][] M = new Node[example.getPatternGraph().size()][example.getWorkingGraph().size()];
 		int[] L = new int[example.getPatternGraph().size()];
 	}
-	
+
 	@Override
 	public List<Match> getMatches(Example example) {
 		this.example = example;
-		
+
 		this.V = new LinkedList<>(example.getPatternGraph());
 		this.W = new LinkedList<>(example.getWorkingGraph());
 		VMM D = new VMM();
-		
+
 		initialize(D);
 		System.out.println(printVMM(D));
 		backtrack(D);
-		
+
 		return matchings;
 	}
-	
+
 	private void initialize(VMM D) {
 		for (Node vi : V) {
 			int i = vi.getIndex();
 			int k = 0;
-			
+
 			for (Node wj : W) {
 				if (nodeCompare(wj, vi)) {
 					D.M[i][k] = wj;
 					k = k + 1;
 				}
 			}
-			
+
 			D.L[i] = k;
 		}
 	}
-	
-	private boolean newMatch = false; 						// FIXME
-	
+
 	private void backtrack(VMM D) {
-		
+
 		if (extendable(D)) {
 			Node vi = pickVertex(D);
-			
-			if (vi != null) {								// FIXME
+
+			if (vi != null) {
 				Node[] Z = getMappableVertices(vi, D);
 
 				for (Node wj : Z) {
@@ -105,63 +101,72 @@ public class KrissinelAlgorithm2 implements IMatchingEngine {
 					newMatch = false;
 				}
 
-				V.remove(vi);
-				VMM D1 = refine(D, vi);						// FIXME
-				backtrack(D1);
-				V.add(vi);
+//				for (int a = 0; a < vi.getAdjacent().size(); ++a) {
+//					Node viadj = vi.getAdjacent().remove(a);
+//					int ar = viadj.getAdjacent().indexOf(vi);
+//					viadj.getAdjacent().remove(ar);
+//
+//					backtrack(D);
+//
+//					vi.getAdjacent().add(a, viadj);
+//					viadj.getAdjacent().add(ar, vi);
+//				}
+
+				 V.remove(vi);
+				
+				 VMM D1 = refine(D, vi);
+				 backtrack(D1);
+				
+				 V.add(vi);
 			} else {
-				if (newMatch) {									// FIXME
+				if (newMatch) {
 					newMatch = false;
-					nmax = Math.max(nmax, X.size());
 					output();
 				}
 			}
 		} else {
-			if (newMatch) {									// FIXME
+			if (newMatch) {
 				newMatch = false;
-				nmax = Math.max(nmax, X.size());
 				output();
 			}
 		}
 	}
-	
+
 	private boolean extendable(VMM D) {
 		int q = X.size(); // actual match size
 		int s = X.size(); // actual match size + free mappable nodes
-		
+
 		for (Node vi : V) {
 			int i = vi.getIndex();
-			
+
 			// All nodes that have not yet been assigned:
 			if (!X.contains(vi)) {
 				// Count of mappable nodes for vi:
 				int Li = D.L[i];
-				
+
 				if (Li > 0) {
 					s = s + 1;
 				}
 			}
 		}
-		
-		// s > q 			=> extendible
-		// s >= n0 			=> potentially extendible to match greater (or equal) then required minimum
-		// (?) s >= nmax 	=> potentially extendible to match greater (or equal) then the last greatest match that was found 
-//		if ((s >= Math.max(n0, nmax)) && (s > q)) {								// FIXME
+
+		// s > q => extendible
+		// s >= n0 => potentially extendible to match greater (or equal) then required minimum
 		if ((s >= n0) && (s > q)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	private Node pickVertex(VMM D) {
-		
+
 		// Pick the vertex with the minimal number of candidates:
 		Node vimin = null;
 		int Li = Integer.MAX_VALUE;
-		
+
 		for (Node xi : X) {
-			for (Node vi : xi.getAdjacent()) {	// FIXME
+			for (Node vi : xi.getAdjacent()) {
 
 				// All nodes that have not yet been assigned:
 				if (!X.contains(vi)) {
@@ -174,35 +179,37 @@ public class KrissinelAlgorithm2 implements IMatchingEngine {
 				}
 			}
 		}
-		
-		// FIXME!
+
+		// FIXME: Find minimum...
 		if (X.isEmpty()) {
 			return V.get(0);
 		}
-		
+
 		return vimin;
 	}
-	
+
 	private Node[] getMappableVertices(Node vi, VMM D) {
 		int i = vi.getIndex();
 		return Arrays.copyOf(D.M[i], D.L[i]);
 	}
-	
+
 	private VMM refine(VMM D, Node vi) {
-		
+
 		// Remove vi from matrix:
 		int i = vi.getIndex();
-		
+
 		// Update all nodes that are adjacent to vi:
 		for (Node wi : D.M[i]) {
 			if (wi != null) {
+				
 				// wi adjacent to all remaining vi?
 				boolean induced = true;
-				
+
+				// FIXME: Should always be fulfilled due to refine(D)!?
 				for (Node adjacent : vi.getAdjacent()) {
 					if (V.contains(adjacent)) {
 						induced = false;
-						
+
 						for (Node wiAdj : wi.getAdjacent()) {
 							if (wiAdj.getLabel() == adjacent) {
 								induced = true;
@@ -216,11 +223,11 @@ public class KrissinelAlgorithm2 implements IMatchingEngine {
 					for (Node adjacent : vi.getAdjacent()) {
 						if (V.contains(adjacent)) {
 							int q = 0;
-							
+
 							for (int j = 0; j < D.M[adjacent.getIndex()].length; ++j) {
 								Node mappable = D.M[adjacent.getIndex()][j];
 								D.M[adjacent.getIndex()][j] = null;
-								
+
 								if (mappable != null) {
 									if (!wi.getAdjacent().contains(mappable)) {
 										D.M[adjacent.getIndex()][q] = mappable;
@@ -228,138 +235,140 @@ public class KrissinelAlgorithm2 implements IMatchingEngine {
 									}
 								}
 							}
-							
+
 							D.L[adjacent.getIndex()] = q;
 						}
 					}
 				}
 			}
 		}
-		
+
 		D.L[i] = 0;
 		D.M[i] = new Node[0];
-		
-		// TEST:
+
+		////////////////////// TEST //////////////////////
 		for (int v = 0; v < D.M.length; ++v) {
 			int q = 0;
-			
+
 			for (int w = 0; w < D.M[v].length; ++w) {
-				if (D.M[v][q] != null) {
+				if (D.M[v][w] != null) {
 					++q;
 				}
 			}
-			
+
 			if (q != D.L[v]) {
 				System.err.println("FAILED!");
 			}
 		}
-		//////////////////////
-		
+		//////////////////////////////////////////////////
+
 		return D;
 	}
-	
+
 	private VMM refine(VMM D) {
-		
+
 		VMM D1 = new VMM();
 		Node[][] T = D1.M;
 		int[] N = D1.L;
-		
+
 		// last/new added node:
-		// int q = X.size(); 
-		
+		// int q = X.size();
+
 		Node xq = X.getLast();
 		Node yq = Y.getLast();
-		
+
 		for (Node vi : V) {
 			int i = vi.getIndex();
-			
+
 			// All nodes that have not yet been assigned:
 			if (!X.contains(vi)) {
-				
-				// Filer all nodes (vi) that are adjacent to the last assigned node (xq):
+
+				// Filer all nodes (vi) that are adjacent to the last assigned
+				// node (xq):
 				// => induced sub-graph matching!
 				if (vi.isAdjacent(xq)) {
 					int l = 0;
-					
+
 					for (int j = 0; j < D.L[i]; j++) {
 						Node wj = D.M[i][j];
-						
+
 						if (edgeCompare(vi, xq, wj, yq)) {
 							T[i][l] = wj;
-							l = l + 1; 
+							l = l + 1;
 						}
 					}
-					
+
 					N[i] = l;
 				} else {
-					
+
 					// Copy old matrix:
 					N[i] = D.L[i];
-					T[i] = Arrays.copyOf(D.M[i], N[i]);;
+					T[i] = Arrays.copyOf(D.M[i], N[i]);
+					;
 				}
 			}
 		}
-		
-		// TEST:
+
+		////////////////////// TEST //////////////////////
 		for (int v = 0; v < D1.M.length; ++v) {
 			int q = 0;
-			
+
 			for (int w = 0; w < D1.M[v].length; ++w) {
-				if (D1.M[v][q] != null) {
+				if (D1.M[v][w] != null) {
 					++q;
 				}
 			}
-			
+
 			if (q != D1.L[v]) {
 				System.err.println("FAILED!");
 			}
 		}
-		//////////////////////
-		
+		//////////////////////////////////////////////////
+
 		return D1;
 	}
 
 	private boolean nodeCompare(Node xi, Node yi) {
 		return (xi.getLabel() == yi);
 	}
-	
+
 	private boolean edgeCompare(Node xi, Node xj, Node yi, Node yj) {
-		assert nodeCompare(xi, yi) && nodeCompare(xj, yj); 
+		assert nodeCompare(xi, yi) && nodeCompare(xj, yj);
 		return xi.isAdjacent(xj) && yi.isAdjacent(yj);
 	}
-	
+
 	private void output() {
 		Match newMatch = new Match();
 		newMatch.setMatch(new ArrayList<>(Y));
 		matchings.add(newMatch);
 	}
-	
+
 	public String printVMM(VMM D) {
 		StringBuffer print = new StringBuffer();
-		
+
 		for (int i = 0; i < D.L.length; ++i) {
-			
+
 			// Pattern-Node:
 			Node vi = example.getPatternGraph().get(i);
-			
+
 			print.append(vi);
 			appendFill(print, vi + "", 3);
 			print.append(" | ");
-			
+
 			// Match:
-			String yiName = ""; 
-			
+			String yiName = "";
+
 			for (Node yi : Y) {
 				if (yi.getLabel() == vi) {
 					yiName = yi.getName();
 					break;
 				}
 			}
-			
+
 			print.append(yiName);
 			appendFill(print, yiName, 3);
 			print.append(" | ");
-			
+
 			// Size:
 			print.append(D.L[i]);
 			appendFill(print, D.L[i] + "", 3);
@@ -367,20 +376,20 @@ public class KrissinelAlgorithm2 implements IMatchingEngine {
 
 			// Matrix:
 			Node[] mappable = D.M[i];
-			
+
 			for (Node node : mappable) {
 				if (node != null) {
 					print.append(node + " ");
 					appendFill(print, node.toString(), 7);
 				}
 			}
-			
+
 			print.append("\n");
 		}
-		
+
 		return print.toString();
 	}
-	
+
 	private void appendFill(StringBuffer print, String s, int length) {
 		for (int j = (length - s.length()); j > 0; --j) {
 			print.append(" ");
