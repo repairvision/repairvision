75b9542ebec840412fee7b2a726353e6c1994978
mohrndorf <manuel.ohrndorf@gmx.net>
2016-11-17 00:30:26 +0000

    simple rulebase for embeddings
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2437 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.repair.api.cpo/src/org/sidiff/consistency/repair/api/cpo/CPORepairFacade.java
+++ plugins/org.sidiff.consistency.repair.api.cpo/src/org/sidiff/consistency/repair/api/cpo/CPORepairFacade.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -13,6 +14,8 @@ import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.henshin.interpreter.EGraph;
 import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
 import org.eclipse.emf.henshin.model.Rule;
+import org.sidiff.common.logging.LogEvent;
+import org.sidiff.common.logging.LogUtil;
 import org.sidiff.consistency.common.debug.DebugUtil;
 import org.sidiff.consistency.repair.api.IRepair;
 import org.sidiff.consistency.repair.api.IRepairFacade;
@@ -20,6 +23,7 @@ import org.sidiff.consistency.repair.api.cpo.lifting.BasicCPOLifting;
 import org.sidiff.consistency.repair.api.cpo.util.StatisticUtil;
 import org.sidiff.consistency.repair.api.matching.EditOperationMatching;
 import org.sidiff.consistency.repair.complement.construction.ComplementRule;
+import org.sidiff.consistency.repair.complement.cpo.embedding.EmbeddingRulebase;
 import org.sidiff.consistency.repair.complement.cpo.matching.ComplementFinder;
 import org.sidiff.consistency.repair.complement.repair.RepairOperation;
 import org.sidiff.difference.lifting.recognitionengine.IRecognitionEngine;
@@ -75,73 +79,107 @@ public class CPORepairFacade implements IRepairFacade<CPORepairJob, CPORepairSet
 			}
 		}
 		
-		// Calculate difference:
-		lifting.findSubEditRules(modelA, modelB, settings);
+		CountDownLatch sync = new CountDownLatch(2);
 		
+		// Calculate difference:
+		Thread differenceThread = new Thread() {
+			public void run() {
+				lifting.findSubEditRules(modelA, modelB, settings);
+				sync.countDown();
+			}
+		};
+		differenceThread.start();
+		
+		// Calculate sub-rule to CPO embeddings:
+		EmbeddingRulebase[] embeddingsResult = new EmbeddingRulebase[1];
+		
+		Thread embeddingsThread = new Thread() {
+			public void run() {
+				EmbeddingRulebase embeddings = new EmbeddingRulebase(settings.getCPOEditRules(), settings.getSubEditRules());
+				embeddingsResult[0] = embeddings;
+				sync.countDown();
+			}
+		};
+		embeddingsThread.start();
+		
+		// Synchronize calculations:
+		try {
+			sync.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		// Get sub-rule to CPO embeddings:
+		EmbeddingRulebase embeddings = embeddingsResult[0];
+		
+		if (DebugUtil.isActive) {
+			LogUtil.log(LogEvent.NOTICE, embeddings.print());
+		}
+		
+		// Calculate repairs:
 		SymmetricDifference difference = lifting.getDifference();
 		IRecognitionEngine recognitionEngine = lifting.getRecognitionEngine();
 		ILiftingRuleBase rulebases_sub = lifting.getRulebasesSub();
 		
-		// TODO: if (difference.getChangeSets().size() > 0) 
-			
-		// TODO: Support differences without resource...
 		Resource differenceResource = differenceRSS.createResource(URI.createURI(""));
 		differenceResource.getContents().add(difference);
 		
-		// Repair application:
-		EngineImpl henshinEngine = new EngineImpl();
-		
-		// Use the graph of the recognition engine or with merged imports!
-		EGraph modelBGraph = recognitionEngine.getGraphModelB();
-		
-		// Calculate repairs:
-		long calculateComplements = System.currentTimeMillis();
-		
-		ComplementFinder complementFinder = new ComplementFinder(
-				recognitionEngine.getRecognitionRuleApplications(), rulebases_sub,
-				settings.getSubEditRules(), settings.getCPOEditRules(), 
-				difference, modelBGraph);
-		
 		Map<Rule, List<IRepair>> repairs = new LinkedHashMap<>();
 		
-		if (DebugUtil.statistic) {
-			System.out.println("#DONE# Complement Calculation: " + (System.currentTimeMillis() - calculateComplements) + "ms");
-		}
-		
-		long calculateRepairs = System.currentTimeMillis();
-		
-		for (Rule cpEditRule : complementFinder.getSourceRules()) {
-			List<IRepair> repairsPerRule = new ArrayList<>();
+		if (difference.getChangeSets().size() > 0) {
+			
+			// Repair application:
+			EngineImpl henshinEngine = new EngineImpl();
+			
+			// Use the graph of the recognition engine or with merged imports!
+			EGraph modelBGraph = recognitionEngine.getGraphModelB();
+			
+			// Calculate repairs:
+			long calculateComplements = System.currentTimeMillis();
+			
+			ComplementFinder complementFinder = new ComplementFinder(
+					recognitionEngine.getRecognitionRuleApplications(), rulebases_sub,
+					settings.getSubEditRules(), embeddings, 
+					difference, modelBGraph);
+			
+			if (DebugUtil.statistic) {
+				System.out.println("#DONE# Complement Calculation: " + (System.currentTimeMillis() - calculateComplements) + "ms");
+			}
+			
+			long calculateRepairs = System.currentTimeMillis();
+			
+			for (Rule cpEditRule : complementFinder.getSourceRules()) {
+				List<IRepair> repairsPerRule = new ArrayList<>();
 
-			for(ComplementRule complement : complementFinder.getComplementRules(cpEditRule)) {
-				if (complement.getComplementingChanges().size() > 0) {
-					for (EditOperationMatching preMatch : complement.getComplementMatches()) {
-						repairsPerRule.add(new RepairOperation(complement, preMatch));
+				for(ComplementRule complement : complementFinder.getComplementRules(cpEditRule)) {
+					if (complement.getComplementingChanges().size() > 0) {
+						for (EditOperationMatching preMatch : complement.getComplementMatches()) {
+							repairsPerRule.add(new RepairOperation(complement, preMatch));
+						}
+						complement.initialize(henshinEngine, modelBGraph);
 					}
-					complement.initialize(henshinEngine, modelBGraph);
+				}
+
+				if (!repairsPerRule.isEmpty()) {
+					repairs.put(cpEditRule, repairsPerRule);
 				}
 			}
-
-			if (!repairsPerRule.isEmpty()) {
-				repairs.put(cpEditRule, repairsPerRule);
+			
+			if (DebugUtil.statistic) {
+				System.out.println("#DONE# Calculate Repairs: " + (System.currentTimeMillis() - calculateRepairs) + "ms");
+				System.out.println("#DONE# Full Repair Job: " + (System.currentTimeMillis() - repairJobTime) + "ms");
+				
+				StatisticUtil.analyzeSetup(settings.getSubEditRules(), settings.getCPOEditRules(), difference);
 			}
 		}
-
+			
 		// Create repair job:
 		CPORepairJob repairJob = new CPORepairJob();
 		repairJob.setDifference(differenceResource);
 		repairJob.setModelA(modelA);
 		repairJob.setModelB(modelB);
 		repairJob.setRepairs(repairs);
-		// repairJob.setValidations(); // TODO: Add a common interface...
 
-		if (DebugUtil.statistic) {
-			System.out.println("#DONE# Calculate Repairs: " + (System.currentTimeMillis() - calculateRepairs) + "ms");
-			System.out.println("#DONE# Full Repair Job: " + (System.currentTimeMillis() - repairJobTime) + "ms");
-			
-			StatisticUtil.analyzeSetup(settings.getSubEditRules(), settings.getCPOEditRules(), difference);
-		}
-		
 		return repairJob;
 	}
 }
