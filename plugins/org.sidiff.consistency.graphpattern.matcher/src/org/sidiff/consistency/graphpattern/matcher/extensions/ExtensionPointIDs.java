0d7660353ccf3ab50f263038fda5edb5f9f0c97e
mohrndorf <manuel.ohrndorf@gmx.net>
2016-12-02 14:36:42 +0000

    refactoring
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2493 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.graphpattern.matcher/src/org/sidiff/consistency/graphpattern/matcher/extensions/ExtensionPointIDs.java
+++
@@ -1,6 +0,0 @@
-package org.sidiff.consistency.graphpattern.matcher.extensions;
-
-public class ExtensionPointIDs {
-	
-	public static final String MATCHER_FACTORY = "org.sidiff.consistency.graphpattern.matcher.factory";
-}
