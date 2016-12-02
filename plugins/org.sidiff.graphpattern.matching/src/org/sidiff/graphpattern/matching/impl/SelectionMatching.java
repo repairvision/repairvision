75320ff2470d38f02e6e339c81fc7b366cd9131d
mohrndorf <manuel.ohrndorf@gmx.net>
2016-12-02 14:39:12 +0000

    refactoring
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2501 db72bd86-da5d-42d1-ad72-61f8f7c676a0

---
+++ plugins/org.sidiff.graphpattern.matching/src/org/sidiff/graphpattern/matching/impl/SelectionMatching.java
@@ -0,0 +1,51 @@
+package org.sidiff.graphpattern.matching.impl;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import org.eclipse.emf.ecore.EObject;
+import org.sidiff.consistency.graphpattern.NodePattern;
+import org.sidiff.graphpattern.matching.IMatching;
+import org.sidiff.graphpattern.wgraph.util.WGraph;
+
+/**
+ * A view that shows the selection of the data store as a match.
+ * 
+ * @author Manuel Ohrndorf
+ */
+public abstract class SelectionMatching implements IMatching {
+
+	protected boolean selectionModified = false;
+	
+	@Override
+	public Iterator<EObject> getMatch(NodePattern node) {
+		if (!selectionModified) {
+			return WGraph.getDataStore(node).getMatchSelection().getSelectedMatches();
+		} else {
+			throw new ConcurrentModificationException();
+		}
+	}
+	
+	@Override
+	public EObject getFirstMatch(NodePattern node) {
+		if (!selectionModified) {
+			Iterator<EObject> matchIt = getMatch(node);
+			
+			if (matchIt.hasNext()) {
+				return matchIt.next();
+			} else {
+				return null;
+			}
+		} else {
+			throw new ConcurrentModificationException();
+		}
+	}
+
+	public boolean isSelectionModified() {
+		return selectionModified;
+	}
+
+	public void setSelectionModified(boolean selectionModified) {
+		this.selectionModified = selectionModified;
+	}
+}
