37f18d18a85ccc9631992685ab698ea1ce4aa52a
mohrndorf <manuel.ohrndorf@gmx.net>
2016-12-05 23:29:21 +0000

    write out difference and  recognition rule for debugging
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2603 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.graphpattern.edit/gensrc/org/sidiff/graphpattern/provider/EdgePatternItemProvider.java
+++ plugins/org.sidiff.graphpattern.edit/gensrc/org/sidiff/graphpattern/provider/EdgePatternItemProvider.java
@@ -180,6 +180,36 @@ public class EdgePatternItemProvider
 	}
 
 	/**
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object) {
+		if (childrenFeatures == null) {
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(GraphpatternPackage.Literals.EDGE_PATTERN__TARGET);
+		}
+		return childrenFeatures;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child) {
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
+	}
+
+	/**
 	 * This returns EdgePattern.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
