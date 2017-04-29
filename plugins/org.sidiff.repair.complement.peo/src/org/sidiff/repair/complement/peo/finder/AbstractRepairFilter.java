96f76b5d9c6ee3c1e9b88ec7edb5183fab2ff777
mohrndorf <manuel.ohrndorf@gmx.net>
2017-04-29 14:08:44 +0000

    extension: abstract repair -> delete root element
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@2932 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.repair.complement.peo/src/org/sidiff/repair/complement/peo/finder/AbstractRepairFilter.java
+++ plugins/org.sidiff.repair.complement.peo/src/org/sidiff/repair/complement/peo/finder/AbstractRepairFilter.java
@@ -8,14 +8,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.henshin.model.Attribute;
 import org.eclipse.emf.henshin.model.Edge;
 import org.eclipse.emf.henshin.model.GraphElement;
 import org.eclipse.emf.henshin.model.Node;
 import org.sidiff.common.emf.access.EMFModelAccess;
+import org.sidiff.common.emf.access.Scope;
 import org.sidiff.repair.api.matching.EditOperationMatching;
 import org.sidiff.repair.validation.IConstraint;
 import org.sidiff.repair.validation.fix.IRepairDecision;
@@ -33,8 +37,12 @@ public class AbstractRepairFilter {
 	private List<Validation> validations = new ArrayList<>();
 
 	public AbstractRepairFilter(Resource model, boolean storeValidation) {
-		List<IConstraint> consistencyRules = ConstraintLibraryUtil.getConsistencyRules(
-				 ConstraintLibraryRegistry.getLibraries(EMFModelAccess.getDocumentType(model)));
+		List<IConstraint> consistencyRules = new ArrayList<>();
+		
+		for (String docType : EMFModelAccess.getDocumentTypes(model, Scope.RESOURCE)) {
+			consistencyRules.addAll(ConstraintLibraryUtil.getConsistencyRules(
+					ConstraintLibraryRegistry.getLibraries(docType)));
+		}
 
 		BatchValidationIterator validationIterator = new BatchValidationIterator(
 				model, consistencyRules, false, true, false);
@@ -131,26 +139,45 @@ public class AbstractRepairFilter {
 
 		for (GraphElement change : changes) {
 
-			// Abstract repairs only consider edges:
+			// Abstract repairs consider only edges and attributes:
 			if (change instanceof Edge) {
-				Node contextNode = ((Edge) change).getSource();
+				EClass sourceContextType = ((Edge) change).getSource().getType();
+				EReference referenceType = ((Edge) change).getType();
 
 				// Delete:
 				if (change.getGraph().isLhs()) {
-					if (isRepair(RepairType.DELETE, contextNode.getType(), ((Edge) change).getType())) {
+					if (isRepair(RepairType.DELETE, sourceContextType, referenceType)) {
 						return true;
+					}
+					
+					// Repair which deletes the root element of a validation:
+					if (referenceType.isContainment() && (referenceType.getEOpposite() == null)) {
+						EClass targetContextType = ((Edge) change).getTarget().getType();
+						
+						if (isRepair(RepairType.DELETE, targetContextType, referenceType)) {
+							return true;
+						}
 					}
 				}
 
 				// Create:
 				else if (change.getGraph().isRhs()) {
-					if (isRepair(RepairType.ADD, contextNode.getType(), ((Edge) change).getType())) {
+					if (isRepair(RepairType.ADD, sourceContextType, referenceType)) {
 						return true;
 					}
 				}
 
 				else {
 					assert false : "We should never get here...!";
+				}
+			}
+			
+			else if (change instanceof Attribute) {
+				EClass contextType = ((Attribute) change).getNode().getType();
+				EAttribute attributeType = ((Attribute) change).getType();
+				
+				if (isRepair(RepairType.MODIFY, contextType, attributeType)) {
+					return true;
 				}
 			}
 		}
@@ -170,31 +197,38 @@ public class AbstractRepairFilter {
 
 		for (GraphElement change : changes) {
 
-			// Abstract repairs only consider edges:
+			// Abstract repairs consider only edges and attributes:
 			if (change instanceof Edge) {
-				
-				// Get the context node of the edge:
-				Node contextNode = getLHS(((Edge) change).getSource());
-				
-				if (contextNode == null) {
-					contextNode = getLHS(((Edge) change).getTarget());
-				}
+				EReference referenceType = ((Edge) change).getType();
 				
 				// Get the context object of the edge:
-				EObject contextObject = prematch.getMatch().getNodeTarget(contextNode);
+				Node sourceContextNode = getLHS(((Edge) change).getSource());
+				EObject sourceContextObject = prematch.getMatch().getNodeTarget(sourceContextNode);
 				
-				if (contextObject != null) {
+				if (sourceContextObject != null) {
 					
 					// Delete:
 					if (change.getGraph().isLhs()) {
-						if (isRepair(RepairType.DELETE, contextObject, ((Edge) change).getType())) {
+						if (isRepair(RepairType.DELETE, sourceContextObject, referenceType)) {
 							return true;
+						}
+						
+						// Repair which deletes the root element of a validation:
+						if (referenceType.isContainment() && (referenceType.getEOpposite() == null)) {
+							
+							// Get the context object of the edge:
+							Node targetContextNode = getLHS(((Edge) change).getTarget());
+							EObject targetContextObject = prematch.getMatch().getNodeTarget(targetContextNode);
+							
+							if (isRepair(RepairType.DELETE, targetContextObject, referenceType)) {
+								return true;
+							}
 						}
 					}
 					
 					// Create:
 					else if (change.getGraph().isRhs()) {
-						if (isRepair(RepairType.ADD, contextObject, ((Edge) change).getType())) {
+						if (isRepair(RepairType.ADD, sourceContextObject, referenceType)) {
 							return true;
 						}
 					}
@@ -203,7 +237,20 @@ public class AbstractRepairFilter {
 						assert false : "We should never get here...!";
 					}
 				}
-
+			}
+			
+			else if (change instanceof Attribute) {
+				EAttribute attributeType = ((Attribute) change).getType();
+				
+				// Get the context object of the edge:
+				Node contextNode = getLHS(((Attribute) change).getNode());
+				EObject contextObject = prematch.getMatch().getNodeTarget(contextNode);
+				
+				if (contextObject != null) {
+					if (isRepair(RepairType.MODIFY, contextObject, attributeType)) {
+						return true;
+					}
+				}
 			}
 		}
 
