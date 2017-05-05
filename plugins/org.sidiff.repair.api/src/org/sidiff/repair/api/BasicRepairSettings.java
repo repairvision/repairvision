9edcd5fa7e9ff437449bfd5d0059e0770113e4e0
mohrndorf <manuel.ohrndorf@gmx.net>
2017-05-05 20:43:57 +0000

    migration: concept of master thesis
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3002 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.repair.api/src/org/sidiff/repair/api/BasicRepairSettings.java
+++ plugins/org.sidiff.repair.api/src/org/sidiff/repair/api/BasicRepairSettings.java
@@ -5,7 +5,7 @@ public class BasicRepairSettings implements IRepairSettings {
 	/**
 	 * Save the calculated model difference.
 	 */
-	private boolean saveDifference = false;
+	private boolean saveDifference = true;
 
 	@Override
 	public boolean saveDifference() {
