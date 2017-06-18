2f43813ba966151ed5940d116a12e7bda3e71826
mohrndorf <manuel.ohrndorf@gmx.net>
2017-06-18 22:34:40 +0000

    stub: edit rule generator
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/evaluation@3235 db72bd86-da5d-42d1-ad72-61f8f7c676a0

---
+++ plugins.evaluation/org.sidiff.repair.history.editrules/src/org/sidiff/repair/history/editrules/generator/HistoryEditRuleGenerator.java
@@ -0,0 +1,64 @@
+package org.sidiff.repair.history.editrules.generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.henshin.model.Module;
+import org.sidiff.repair.historymodel.History;
+import org.sidiff.repair.historymodel.Version;
+
+public class HistoryEditRuleGenerator {
+
+	protected History history;
+	
+	protected List<Module> rulebase;
+	
+	public HistoryEditRuleGenerator(History history) {
+		this.history = history;
+	}
+	
+	public void start() {
+		for (int i = 0; i < history.getVersions().size(); i++) {
+			Version vA = history.getVersions().get(i);
+			Version vB = history.getVersions().get(i + 1);
+			List<Module> editRules = calculateEditRules(vA, vB);
+			integrateIntoRulebase(editRules);
+		}
+	}
+	
+	protected List<Module> calculateEditRules(Version vA, Version vB) {
+		List<Module> editRules = new ArrayList<>();
+		
+		// Calculate all validation scopes:
+		
+		// Calculate difference:
+		
+		// Find validation pairs:
+		
+		// Ignore scopes with no changes:
+		
+		// Create change-sets:
+		// > Find nested scopes by changes.
+		// > Sum nested and parent scopes.
+		
+		// "Record" edit-rules:
+		
+		return editRules;
+	}
+	
+	protected void integrateIntoRulebase(List<Module> editRules) {
+		
+		// Find similar/nested edit-rules:
+		// > Make rules as abstract as possible.
+		// > reduce to minimal context.
+		
+		// Merge matched (as concrete as possible) edit-rules:
+		
+		rulebase.addAll(editRules);
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
---
+++ plugins.evaluation/org.sidiff.repair.history.editrules/src/org/sidiff/repair/history/editrules/generator/HistoryToEditRuleGenerator.java
@@ -0,0 +1,24 @@
+package org.sidiff.repair.history.editrules.generator;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.sidiff.common.emf.modelstorage.EMFHandlerUtil;
+import org.sidiff.repair.historymodel.History;
+
+public class HistoryToEditRuleGenerator extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		History history = EMFHandlerUtil.getSelection(event, History.class);
+		
+		if (history != null) {
+			HistoryEditRuleGenerator generator = new HistoryEditRuleGenerator(history);
+			generator.start();
+		}
+		
+		return null;
+	}
+	
+}
