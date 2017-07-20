36d40602ee3c646255f12bcc0bf6d1c554b517ff
mohrndorf <manuel.ohrndorf@gmx.net>
2017-07-20 10:01:55 +0000

    fix: get containment references for remove objects
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/evaluation@3279 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins.evaluation/org.sidiff.repair.history.editrules/src/org/sidiff/repair/history/editrules/learning/DifferenceNavigation.java
+++ plugins.evaluation/org.sidiff.repair.history.editrules/src/org/sidiff/repair/history/editrules/learning/DifferenceNavigation.java
@@ -69,7 +69,7 @@ public class DifferenceNavigation {
 		for (Change change : getLocalChanges(modelElement)) {
 			if (change instanceof RemoveObject) {
 				return change;
-			} else  if (change instanceof AddObject) {
+			} else if (change instanceof AddObject) {
 				return change;
 			}
 		}
@@ -86,6 +86,12 @@ public class DifferenceNavigation {
 						return localChange;
 					}
 				}
+			} else if (localChange instanceof RemoveReference) {
+				if (((RemoveReference) localChange).getSrc() == element) {
+					if (((RemoveReference) localChange).getType().isContainer()) {
+						return localChange;
+					}
+				}
 			}
 		}
 		
@@ -99,6 +105,12 @@ public class DifferenceNavigation {
 			if (localChange instanceof AddReference) {
 				if (((AddReference) localChange).getTgt() == element) {
 					if (((AddReference) localChange).getType().isContainment()) {
+						return localChange;
+					}
+				}
+			} else if (localChange instanceof RemoveReference) {
+				if (((RemoveReference) localChange).getTgt() == element) {
+					if (((RemoveReference) localChange).getType().isContainment()) {
 						return localChange;
 					}
 				}
