6c712530182b3ddcd7fb17205f70f80feb727ef2
mohrndorf <manuel.ohrndorf@gmx.net>
2017-12-08 14:13:25 +0000

    migration to history analysis / repository connector
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3630 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.repair.ui.peo/src/org/sidiff/repair/ui/peo/integrated/IntegratedRepairApplication.java
+++ plugins/org.sidiff.repair.ui.peo/src/org/sidiff/repair/ui/peo/integrated/IntegratedRepairApplication.java
@@ -1,7 +1,5 @@
 package org.sidiff.repair.ui.peo.integrated;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -10,12 +8,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.henshin.interpreter.Match;
 import org.eclipse.emf.henshin.model.Rule;
@@ -25,10 +18,11 @@ import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
-import org.sidiff.common.emf.modelstorage.EMFStorage;
 import org.sidiff.consistency.common.emf.DocumentType;
 import org.sidiff.consistency.common.ui.util.WorkbenchUtil;
 import org.sidiff.difference.technical.api.settings.DifferenceSettings;
+import org.sidiff.history.analysis.tracing.HistoryModelDatabase;
+import org.sidiff.history.analysis.validation.FOLValidator;
 import org.sidiff.integration.editor.access.IntegrationEditorAccess;
 import org.sidiff.integration.editor.extension.IEditorIntegration;
 import org.sidiff.repair.api.IRepairFacade;
@@ -36,18 +30,11 @@ import org.sidiff.repair.api.IRepairPlan;
 import org.sidiff.repair.api.peo.PEORepairJob;
 import org.sidiff.repair.api.peo.PEORepairSettings;
 import org.sidiff.repair.editrules.library.RulebaseLibrary;
-import org.sidiff.repair.historymodel.History;
-import org.sidiff.repair.historymodel.ValidationError;
-import org.sidiff.repair.historymodel.Version;
 import org.sidiff.repair.ui.app.impl.EMFResourceRepairApplication;
-import org.sidiff.repair.ui.peo.integrated.app.FOLValidator;
-import org.sidiff.repair.ui.peo.integrated.app.HistoryModelGenerator;
-import org.sidiff.repair.ui.peo.integrated.app.SVNConnector;
 import org.sidiff.repair.ui.util.EditRuleUtil;
 import org.sidiff.validation.constraint.api.ValidationFacade;
 import org.sidiff.validation.constraint.api.util.RepairValidation;
 import org.sidiff.validation.constraint.api.util.Validation;
-import org.sidiff.validation.constraint.interpreter.IConstraint;
 
 public class IntegratedRepairApplication extends EMFResourceRepairApplication<PEORepairJob, PEORepairSettings> {
 
@@ -189,120 +176,7 @@ public class IntegratedRepairApplication extends EMFResourceRepairApplication<PE
 	}
 	
 	private Resource getHistoricModel() {
-		
-		URI historyDatabaseURI = getModelB().getURI().appendFileExtension("history");
-		File historyDatabaseFile = EMFStorage.uriToFile(historyDatabaseURI);
-		History history = null;
-		
-		// Update history database:
-		if (historyDatabaseFile.exists()) {
-			
-			// Load history model:
-			ResourceSet rss = new ResourceSetImpl();
-			Resource historyResource = rss.getResource(historyDatabaseURI, true);
-			
-			history = (History) historyResource.getContents().get(0);
-			
-			// FIXME: Append new versions from repository!
-			
-			// Update current version:
-			Version currentModelVersion = HistoryModelGenerator.generateVersion(
-					history.getVersions().size(), getModelB(), new FOLValidator());
-			
-			history.getVersions().remove(history.getVersions().size() - 1);
-			history.getVersions().add(currentModelVersion);
-			
-			// TODO: Make this incremental:
-			HistoryModelGenerator.generateIntroducedAndResolved(history, new FOLValidator());
-		} 
-		
-		// Create new history database:
-		else {
-			
-			// Check out revisions:
-			SVNConnector vcsConnector = new SVNConnector(getModelB());
-			List<File> revisions = vcsConnector.getRevisions();
-			
-			// Append current version
-			Collections.reverse(revisions);
-			revisions.add(EMFStorage.uriToFile(getModelB().getURI()));
-			
-			// Search inconsistency traces:
-			history = HistoryModelGenerator.generateHistory(
-					getModelB().getURI().toString(), revisions, 
-					new FOLValidator(), settings);
-			
-			try {
-				ResourceSet rss = new ResourceSetImpl();
-				Resource historyResource = rss.createResource(getModelB().getURI().appendFileExtension("history"));
-				historyResource.getContents().add(history);
-				
-				historyResource.save(Collections.emptyMap());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		// Find last consistent version:
-		if (history != null) {
-			Version versionModelB = history.getVersions().get(history.getVersions().size() - 1);
-			ValidationError validationError = getValidationError(inconsistency, versionModelB);
-			Version lastConsistentVersion = getPrecessorRevision(validationError.getIntroducedIn());
-			
-			if (lastConsistentVersion != null) {
-				return lastConsistentVersion.getModel();
-			}
-		}
-			
-		return null;
-	}
-	
-	public static Version getPrecessorRevision(Version version) {
-		History history = (History) version.eContainer();
-		int index = history.getVersions().indexOf(version);
-
-		if ((index - 1) >= 0) {
-			return history.getVersions().get(index - 1);
-		}
-		
-		return null;
-	}
-	
-	public static ValidationError getValidationError(Validation validation, Version model) {
-		
-		for(ValidationError nextValidationError : model.getValidationErrors()) {
-			if (equalsValidation(validation, nextValidationError)) {
-				return nextValidationError;
-			}
-		}
-		
-		return null;
-	}
-	
-	public static boolean equalsValidation(Validation validationA, ValidationError validationB) {
-		
-		if (getValidationID(validationA.getRule()).equalsIgnoreCase(getValidationID(validationB))) {
-			EObject invalidElementA = validationA.getContext();
-			EObject invalidElementB = validationB.getContext();
-
-			if (EcoreUtil.getURI(invalidElementA).fragment().equals(EcoreUtil.getURI(invalidElementB).fragment())) {
-				return true;
-			}
-		}
-			
-		return false;
-	}
-	
-	public static String getValidationID(String name) {
-		return name.replaceAll("[^\\p{Alpha}]", "");
-	}
-	
-	public static String getValidationID(IConstraint validation) {
-		return getValidationID(validation.getName());
-	}
-	
-	public static String getValidationID(ValidationError validation) {
-		return getValidationID(validation.getName());
+		return HistoryModelDatabase.getHistoricConsistentModel(getModelB(), new FOLValidator(), inconsistency, settings);
 	}
 	
 	@Override
