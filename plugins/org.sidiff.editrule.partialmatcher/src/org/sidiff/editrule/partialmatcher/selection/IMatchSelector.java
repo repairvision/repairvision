9031bd326ccd9a65e0a79be69beb2b7a70ba99be
mohrndorf <manuel.ohrndorf@gmx.net>
2018-03-06 14:03:35 +0000

    doc
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3697 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.editrule.partialmatcher/src/org/sidiff/editrule/partialmatcher/selection/IMatchSelector.java
+++ plugins/org.sidiff.editrule.partialmatcher/src/org/sidiff/editrule/partialmatcher/selection/IMatchSelector.java
@@ -8,15 +8,51 @@ import org.sidiff.graphpattern.NodePattern;
 
 public interface IMatchSelector {
 
+	/**
+	 * The first selection for the search pattern.
+	 * 
+	 * @param selectedNode
+	 *            The node that should be selected.
+	 * @param selectedMatch
+	 *            The match for the selected node.
+	 */
 	void initialSelection(NodePattern selectedNode, EObject selectedMatch);
-	
+
+	/**
+	 * All subsequent
+	 * ({@link IMatchSelector#initialSelection(NodePattern, EObject)}) selections
+	 * for the search pattern.
+	 * 
+	 * @param selectedNode
+	 *            The node that should be selected.
+	 * @param selectedMatch
+	 *            The match for the selected node.
+	 */
 	void selection(NodePattern selectedNode, EObject selectedMatch);
-	
+
+	/**
+	 * Undo last selection of the search pattern.
+	 * 
+	 * @param unselectedNode
+	 *            The last selected node.
+	 */
 	void undoSelection(NodePattern unselectedNode);
-	
+
+	/**
+	 * @return <code>true</code> if the search path is recorded; <code>false</code>
+	 *         otherwise.
+	 */
 	boolean isRecording();
-	
+
+	/**
+	 * @param recording
+	 *            <code>true</code> if the search path should be recorded;
+	 *            <code>false</code> otherwise.
+	 */
 	void setRecording(boolean recording);
-	
+
+	/**
+	 * @return The recorded search paths.
+	 */
 	List<List<ActionNode>> getRecording();
 }
