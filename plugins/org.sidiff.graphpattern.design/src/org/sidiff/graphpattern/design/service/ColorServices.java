77a3d876d5e546f72edbd3c2e89d8aabd2cc24fa
mohrndorf <manuel.ohrndorf@gmx.net>
2018-04-04 14:32:29 +0000

    color scheme
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3747 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.graphpattern.design/src/org/sidiff/graphpattern/design/service/ColorServices.java
+++ plugins/org.sidiff.graphpattern.design/src/org/sidiff/graphpattern/design/service/ColorServices.java
@@ -1,24 +1,96 @@
 package org.sidiff.graphpattern.design.service;
 
 import org.eclipse.emf.ecore.EObject;
+import org.sidiff.graphpattern.EdgePattern;
+import org.sidiff.graphpattern.GraphElement;
 import org.sidiff.graphpattern.NodePattern;
 import org.sidiff.graphpattern.Profile;
 import org.sidiff.graphpattern.Stereotype;
 import org.sidiff.graphpattern.profile.IGraphPatternProfile;
 import org.sidiff.graphpattern.profile.IGraphPatternVisualization;
+import org.sidiff.graphpattern.profile.IGraphPatternVisualization.Gradient;
 import org.sidiff.graphpattern.profile.extensions.GraphPatternProfileEntry;
 import org.sidiff.graphpattern.profile.extensions.GraphPatternProfileLibrary;
 
 public class ColorServices {
+	
+	/**
+	 * Default node background color (first gradient color): white
+	 */
+	protected int[] DEFAULT_NODE_BACKGROUND_FROM = {255, 255, 255};
+	
+	/**
+	 * Default node background color (second gradient color): light orange
+	 */
+	protected int[] DEFAULT_NODE_BACKGROUND_TO = {253, 211, 149};
+	
+	/**
+	 * Unmatched node background color (first gradient color): white
+	 */
+	protected int[] UNMATCHED_NODE_BACKGROUND_FROM = {255, 255, 255};
+	
+	/**
+	 * Unmatched node background color (second gradient color): white
+	 */
+	protected int[] UNMATCHED_NODE_BACKGROUND_TO = {255, 255, 255};
+	
+	/**
+	 * Default node frame color: black
+	 */
+	protected int[] DEFAULT_NODE_FRAME = {0, 0, 0};
+	
+	/**
+	 * Default node label color: black
+	 */
+	protected int[] DEFAULT_NODE_LABEL = {0, 0, 0};
 
-	public int getNodeColorR(EObject element) {
+	/**
+	 * Default edge line color: black
+	 */
+	protected int[] DEFAULT_EDGE_LINE = {0, 0, 0};
+	
+	/**
+	 * Default edge label color: black
+	 */
+	protected int[] DEFAULT_EDGE_LABEL = {0, 0, 0};
+	
+	public int getNodeBackgroundToColorR(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.TO)[0];
+	}
+	
+	public int getNodeBackgroundFromColorR(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.FROM)[0];
+	}
+	
+	public int getNodeBackgroundToColorG(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.TO)[1];
+	}
+	
+	public int getNodeBackgroundFromColorG(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.FROM)[1];
+	}
+	
+	public int getNodeBackgroundToColorB(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.TO)[2];
+	}
+	
+	public int getNodeBackgroundFromColorB(EObject element) {
+		return getNodeBackgroundColor(element, Gradient.FROM)[2];
+	}
+	
+	protected int[] getNodeBackgroundColor(EObject element, Gradient gradient) {
 		
 		if (element instanceof NodePattern) {
 			NodePattern node = (NodePattern) element;
 			
 			if (node.getMatching() != null) {
 				if (node.getMatching().size() == 0) {
-					return 255;
+					switch (gradient) {
+					case FROM:
+						return UNMATCHED_NODE_BACKGROUND_FROM;
+					case TO:
+						return UNMATCHED_NODE_BACKGROUND_TO;
+					}
 				}
 			}
 			
@@ -26,63 +98,140 @@ public class ColorServices {
 				IGraphPatternVisualization visualization = getProfileVisualization(node);
 				
 				if (visualization != null) {
-					return visualization.getBackgroundColor(node)[0];
+					return visualization.getNodeBackgroundColor(node, gradient);
 				}
 			}
 		}
 		
-		return 253;
+		switch (gradient) {
+		case FROM:
+			return DEFAULT_NODE_BACKGROUND_FROM;
+		case TO:
+			return DEFAULT_NODE_BACKGROUND_TO;
+		default:
+			return new int[] {255, 255, 255};
+		}
 	}
 	
-	public int getNodeColorG(EObject element) {
+	public int getNodeFrameColorR(EObject element) {
+		return getNodeFrameColor(element)[0];
+	}
+	
+	public int getNodeFrameColorG(EObject element) {
+		return getNodeFrameColor(element)[1];
+	}
+	
+	public int getNodeFrameColorB(EObject element) {
+		return getNodeFrameColor(element)[2];
+	}
+	
+	protected int[] getNodeFrameColor(EObject element) {
 		
 		if (element instanceof NodePattern) {
 			NodePattern node = (NodePattern) element;
-			
-			if (node.getMatching() != null) {
-				if (node.getMatching().size() == 0) {
-					return 255;
-				}
-			}
-			
+
 			if (!node.getStereotypes().isEmpty()) {
 				IGraphPatternVisualization visualization = getProfileVisualization(node);
 				
 				if (visualization != null) {
-					return visualization.getBackgroundColor(node)[1];
+					return visualization.getNodeFrameColor((NodePattern) element);
 				}
 			}
 		}
 		
-		return 211;
+		return DEFAULT_NODE_FRAME;
 	}
 	
-	public int getNodeColorB(EObject element) {
+	public int getNodeLabelColorR(EObject element) {
+		return getNodeLabelColor(element)[0];
+	}
+	
+	public int getNodeLabelColorG(EObject element) {
+		return getNodeLabelColor(element)[1];
+	}
+	
+	public int getNodeLabelColorB(EObject element) {
+		return getNodeLabelColor(element)[2];
+	}
+	
+	protected int[] getNodeLabelColor(EObject element) {
 		
 		if (element instanceof NodePattern) {
 			NodePattern node = (NodePattern) element;
-			
-			if (node.getMatching() != null) {
-				if (node.getMatching().size() == 0) {
-					return 255;
-				}
-			}
-			
+
 			if (!node.getStereotypes().isEmpty()) {
 				IGraphPatternVisualization visualization = getProfileVisualization(node);
 				
 				if (visualization != null) {
-					return visualization.getBackgroundColor(node)[2];
+					return visualization.getNodeLabelColor((NodePattern) element);
 				}
 			}
 		}
 		
-		return 149;
+		return DEFAULT_NODE_LABEL;
 	}
 	
-	protected static IGraphPatternVisualization getProfileVisualization(NodePattern node) {
-		if (!node.getStereotypes().isEmpty()) {
-			Stereotype stereotype = node.getStereotypes().get(0);
+	public int getEdgeLineColorR(EObject element) {
+		return getEdgeLineColor(element)[0];
+	}
+	
+	public int getEdgeLineColorG(EObject element) {
+		return getEdgeLineColor(element)[1];
+	}
+	
+	public int getEdgeLineColorB(EObject element) {
+		return getEdgeLineColor(element)[2];
+	}
+	
+	protected int[] getEdgeLineColor(EObject element) {
+		
+		if (element instanceof EdgePattern) {
+			EdgePattern edge = (EdgePattern) element;
+
+			if (!edge.getStereotypes().isEmpty()) {
+				IGraphPatternVisualization visualization = getProfileVisualization(edge);
+				
+				if (visualization != null) {
+					return visualization.getEdgeLineColor((EdgePattern) element);
+				}
+			}
+		}
+		
+		return DEFAULT_EDGE_LINE;
+	}
+	
+	public int getEdgeLabelColorR(EObject element) {
+		return getEdgeLabelColor(element)[0];
+	}
+	
+	public int getEdgeLabelColorG(EObject element) {
+		return getEdgeLabelColor(element)[1];
+	}
+	
+	public int getEdgeLabelColorB(EObject element) {
+		return getEdgeLabelColor(element)[2];
+	}
+	
+	protected int[] getEdgeLabelColor(EObject element) {
+		
+		if (element instanceof EdgePattern) {
+			EdgePattern edge = (EdgePattern) element;
+
+			if (!edge.getStereotypes().isEmpty()) {
+				IGraphPatternVisualization visualization = getProfileVisualization(edge);
+				
+				if (visualization != null) {
+					return visualization.getEdgeLabelColor((EdgePattern) element);
+				}
+			}
+		}
+		
+		return DEFAULT_EDGE_LABEL;
+	}
+	
+	protected static IGraphPatternVisualization getProfileVisualization(GraphElement element) {
+		if (!element.getStereotypes().isEmpty()) {
+			Stereotype stereotype = element.getStereotypes().get(0);
 			
 			if (stereotype.eContainer() instanceof Profile) {
 				Profile profile = (Profile) stereotype.eContainer();
