f42f1e79672a53fd31fca06e1306216fb1f59f03
mohrndorf <manuel.ohrndorf@gmx.net>
2018-07-12 14:00:16 +0000

    edit rule generation
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3944 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.graphpattern.tools/src/org/sidiff/graphpattern/tools/editrules/EditRuleGenerator.java
+++ plugins/org.sidiff.graphpattern.tools/src/org/sidiff/graphpattern/tools/editrules/EditRuleGenerator.java
@@ -2,6 +2,7 @@ package org.sidiff.graphpattern.tools.editrules;
 
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 import org.sidiff.graphpattern.EdgePattern;
 import org.sidiff.graphpattern.GraphPattern;
@@ -28,45 +29,85 @@ public class EditRuleGenerator {
 		Map<EdgePattern,EdgePattern> edgeMatching = EditRuleGeneratorUtil.getEdgeMatching(nodeMatching);
 		
 		GraphPattern editRule = GraphpatternFactory.eINSTANCE.createGraphPattern();
-		Copier copier = new Copier(); 
+		Copier copier = new Copier();
 		
-		// << delete/preserve >>
-		for (NodePattern preNode : preConstraint.getNodes()) {
-			NodePattern deleteOrContextNode = (NodePattern) copier.copy(preNode);
-			editRule.getNodes().add(deleteOrContextNode);
-			
-			if (!nodeMatching.containsKey(preNode)) {
-				deleteOrContextNode.getStereotypes().add(deleteST);
-			} else {
-				deleteOrContextNode.getStereotypes().add(preserveST);
-			}
-			
-			for (EdgePattern preEdge : preNode.getOutgoings()) {
-				EdgePattern deleteOrContextEdge = (EdgePattern) copier.copy(preEdge);
-				deleteOrContextNode.getOutgoings().add(deleteOrContextEdge);
-				
-				if (edgeMatching.containsKey(preEdge)) {
-					deleteOrContextEdge.getStereotypes().add(deleteST);
-				} else {
-					deleteOrContextEdge.getStereotypes().add(preserveST);
-				}
-			}
-		}
-		
-		// << create >>
+		// << create/preserve >> nodes/edges:
+		// NOTE: Edges will be copied implicitly.
 		for (NodePattern postNode : postConstraint.getNodes()) {
-			if (!nodeMatching.containsValue(postNode)) {
+			if (nodeMatching.containsValue(postNode)) {
+				
+				// << preserve >> node/edges:
+				NodePattern contextNode = (NodePattern) copier.copy(postNode);
+				contextNode.getStereotypes().add(preserveST);
+				editRule.getNodes().add(contextNode);
+				
+				for (EdgePattern createOrContextEdge : contextNode.getOutgoings()) {
+					EdgePattern postEdge = (EdgePattern) copier.get(createOrContextEdge);
+					
+					if (edgeMatching.containsValue(postEdge)) {
+						createOrContextEdge.getStereotypes().add(preserveST);
+					} else {
+						createOrContextEdge.getStereotypes().add(createST);
+					}
+				}
+			} else {
+				
+				// << create >> node/edges:
 				NodePattern createNode = (NodePattern) copier.copy(postNode);
 				createNode.getStereotypes().add(createST);
 				editRule.getNodes().add(createNode);
 				
-				for (EdgePattern postEdge : postNode.getOutgoings()) {
-					if (!edgeMatching.containsKey(postEdge)) {
-						EdgePattern createEdge = (EdgePattern) copier.copy(postEdge);
-						createEdge.getStereotypes().add(createST);
-						createNode.getOutgoings().add(createEdge);
+				for (EdgePattern createEdge : createNode.getOutgoings()) {
+					createEdge.getStereotypes().add(createST);
+				}
+			}
+		}
+		
+		// << delete >> nodes/edges:
+		// NOTE: Edges of create-nodes will be copied implicitly.
+		for (NodePattern preNode : preConstraint.getNodes()) {
+			
+			if (nodeMatching.containsKey(preNode)) {
+				
+				// << delete >> edges
+				NodePattern contextNode = (NodePattern) copier.get(nodeMatching.get(preNode));
+						
+				for (EdgePattern preEdge : preNode.getOutgoings()) {
+					if (!edgeMatching.containsKey(preEdge)) {
+						EdgePattern deleteEdge = (EdgePattern) copier.copy(preEdge);
+						deleteEdge.getStereotypes().add(deleteST);
+						contextNode.getOutgoings().add(deleteEdge);
 					}
 				}
+			} else {
+				
+				// << delete >> node/edges:
+				NodePattern deleteNode = (NodePattern) copier.copy(preNode);
+				deleteNode.getStereotypes().add(deleteST);
+				editRule.getNodes().add(deleteNode);
+				
+				for (EdgePattern deleteEdge : deleteNode.getOutgoings()) {
+					deleteEdge.getStereotypes().add(deleteST);
+				}
+			}
+		}
+		
+		// Set edge targets:
+		for (EObject constraintGraphElement : copier.keySet()) {
+			if (constraintGraphElement instanceof EdgePattern) {
+				EdgePattern constraintEdge = (EdgePattern) constraintGraphElement;
+				EdgePattern editRuleEdge = (EdgePattern) copier.get(constraintEdge);
+				
+				NodePattern constraintTargetNode = constraintEdge.getTarget();
+				NodePattern editRuleTargetNode = null;
+				
+				if (copier.containsKey(constraintTargetNode)) {
+					editRuleTargetNode = (NodePattern) copier.get(constraintTargetNode);
+				} else {
+					editRuleTargetNode = (NodePattern) copier.get(nodeMatching.get(constraintTargetNode));
+				}
+				
+				editRuleEdge.setTarget(editRuleTargetNode);
 			}
 		}
 		
