255eb8293d1a6e68d3822ef88e542e7d92b29169
mohrndorf <manuel.ohrndorf@gmx.net>
2018-07-17 14:40:43 +0000

    remove edit rule postconditions
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@3985 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.graphpattern.profile.henshin/src/org/sidiff/graphpattern/profile/henshin/util/HenshinProfileUtil.java
+++ plugins/org.sidiff.graphpattern.profile.henshin/src/org/sidiff/graphpattern/profile/henshin/util/HenshinProfileUtil.java
@@ -1,9 +1,11 @@
 package org.sidiff.graphpattern.profile.henshin.util;
 
+import static org.sidiff.graphpattern.profile.constraints.ConstraintStereotypes.not;
 import static org.sidiff.graphpattern.profile.henshin.HenshinStereotypes.create;
 import static org.sidiff.graphpattern.profile.henshin.HenshinStereotypes.delete;
 
 import org.sidiff.graphpattern.EdgePattern;
+import org.sidiff.graphpattern.GraphPattern;
 import org.sidiff.graphpattern.NodePattern;
 
 public class HenshinProfileUtil {
@@ -12,6 +14,20 @@ public class HenshinProfileUtil {
 		for (EdgePattern incident : node.getIncident()) {
 			if (incident.getStereotypes().contains(delete) || incident.getStereotypes().contains(create)) {
 				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean hasPostCondition(GraphPattern editRule) {
+		for (NodePattern node : editRule.getNodes()) {
+			for (EdgePattern edge : node.getOutgoings()) {
+				if (edge.getStereotypes().contains(not)) {
+					if (edge.getSource().getStereotypes().contains(create) 
+							|| edge.getTarget().getStereotypes().contains(create)) {
+						return true;
+					}
+				}
 			}
 		}
 		return false;
