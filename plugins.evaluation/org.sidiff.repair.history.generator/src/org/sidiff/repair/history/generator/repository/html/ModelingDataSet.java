5cec8a9083de9422d7d3622dc0b031dab5850b2a
mohrndorf <manuel.ohrndorf@gmx.net>
2018-07-19 14:38:52 +0000

    data set
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/evaluation@4005 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins.evaluation/org.sidiff.repair.history.generator/src/org/sidiff/repair/history/generator/repository/html/ModelingDataSet.java
+++ plugins.evaluation/org.sidiff.repair.history.generator/src/org/sidiff/repair/history/generator/repository/html/ModelingDataSet.java
@@ -25,43 +25,43 @@ public class ModelingDataSet {
 		
 		ModelingDataSet dataSet = new ModelingDataSet();
 		
-//		// https://projects.eclipse.org/projects/modeling.emft.emf-store/developer
-//		dataSet.addProject(localPath, "modeling.emft.emf-store", "http://git.eclipse.org/c/emf-store/org.eclipse.emf.emfstore.core.git",
-//				"/bundles/org.eclipse.emf.emfstore.client/model/client.ecore",
-//				"/bundles/org.eclipse.emf.emfstore.common.model/model/common.ecore",
-//				"/bundles/org.eclipse.emf.emfstore.fuzzy.emf/model/config.ecore",
-//				"/bundles/org.eclipse.emf.emfstore.server.model/model/server.ecore");
-//		
-//		// https://projects.eclipse.org/projects/modeling.mdt.ocl/developer
-//		dataSet.addProject(localPath, "modeling.mdt.ocl", "http://git.eclipse.org/c/ocl/org.eclipse.ocl.git",
-//				"/plugins/org.eclipse.ocl.pivot.uml/model/OCLforUML.ecore",
-//				"/plugins/org.eclipse.ocl.pivot/model/Lookup.ecore",
-//				"/plugins/org.eclipse.ocl.pivot/model/Pivot.ecore",
-//				"/plugins/org.eclipse.ocl.pivot/model/Values.ecore",
-//				"/plugins/org.eclipse.ocl.uml/model/OCLUML.ecore",
-//				"/plugins/org.eclipse.ocl.ecore/model/OCLEcore.ecore",
-//				"/plugins/org.eclipse.ocl.ecore/model/oclstdlib.ecore",
-//				"/plugins/org.eclipse.ocl/model/OCL.ecore",
-//				"/plugins/org.eclipse.ocl/model/OCLCST.ecore");
-//		
-//		// https://projects.eclipse.org/projects/modeling.mmt.qvt-oml/developer
-//		dataSet.addProject(localPath, "modeling.mmt.qvt-oml", "http://git.eclipse.org/c/mmt/org.eclipse.qvto.git",
-//				"/plugins/org.eclipse.m2m.qvt.oml.ecore.imperativeocl/model/ImperativeOCL.ecore",
-//				"/plugins/org.eclipse.m2m.qvt.oml.emf.util/model/MModelURIMap.ecore",
-//				"/plugins/org.eclipse.m2m.qvt.oml/model/QVTOperational.ecore",
-//				"/plugins/org.eclipse.m2m.qvt.oml/model/trace.ecore");
-//		
-//		// https://projects.eclipse.org/projects/technology.ogee/developer
-//		dataSet.addProject(localPath, "technology.ogee", "http://git.eclipse.org/c/ogee/org.eclipse.ogee.git",
-//				"/org.eclipse.ogee.model/model/OData.ecore");
-//		
-//		// https://projects.eclipse.org/projects/modeling.m2t.acceleo/developer
-//		dataSet.addProject(localPath, "modeling.m2t.acceleo", "http://git.eclipse.org/c/acceleo/org.eclipse.acceleo.git",
-//				"/plugins/org.eclipse.acceleo.model/model/mtl.ecore",
-//				"/plugins/org.eclipse.acceleo.model/model/mtlnonstdlib.ecore",
-//				"/plugins/org.eclipse.acceleo.model/model/mtlstdlib.ecore",
-//				"/plugins/org.eclipse.acceleo.profiler/model/profiler.ecore",
-//				"/plugins/org.eclipse.acceleo.ui.interpreter.completeocl/model/evaluation_result.ecore");
+		// https://projects.eclipse.org/projects/modeling.emft.emf-store/developer
+		dataSet.addProject(localPath, "modeling.emft.emf-store", "http://git.eclipse.org/c/emf-store/org.eclipse.emf.emfstore.core.git",
+				"/bundles/org.eclipse.emf.emfstore.client/model/client.ecore",
+				"/bundles/org.eclipse.emf.emfstore.common.model/model/common.ecore",
+				"/bundles/org.eclipse.emf.emfstore.fuzzy.emf/model/config.ecore",
+				"/bundles/org.eclipse.emf.emfstore.server.model/model/server.ecore");
+		
+		// https://projects.eclipse.org/projects/modeling.mdt.ocl/developer
+		dataSet.addProject(localPath, "modeling.mdt.ocl", "http://git.eclipse.org/c/ocl/org.eclipse.ocl.git",
+				"/plugins/org.eclipse.ocl.pivot.uml/model/OCLforUML.ecore",
+				"/plugins/org.eclipse.ocl.pivot/model/Lookup.ecore",
+				"/plugins/org.eclipse.ocl.pivot/model/Pivot.ecore",
+				"/plugins/org.eclipse.ocl.pivot/model/Values.ecore",
+				"/plugins/org.eclipse.ocl.uml/model/OCLUML.ecore",
+				"/plugins/org.eclipse.ocl.ecore/model/OCLEcore.ecore",
+				"/plugins/org.eclipse.ocl.ecore/model/oclstdlib.ecore",
+				"/plugins/org.eclipse.ocl/model/OCL.ecore",
+				"/plugins/org.eclipse.ocl/model/OCLCST.ecore");
+		
+		// https://projects.eclipse.org/projects/modeling.mmt.qvt-oml/developer
+		dataSet.addProject(localPath, "modeling.mmt.qvt-oml", "http://git.eclipse.org/c/mmt/org.eclipse.qvto.git",
+				"/plugins/org.eclipse.m2m.qvt.oml.ecore.imperativeocl/model/ImperativeOCL.ecore",
+				"/plugins/org.eclipse.m2m.qvt.oml.emf.util/model/MModelURIMap.ecore",
+				"/plugins/org.eclipse.m2m.qvt.oml/model/QVTOperational.ecore",
+				"/plugins/org.eclipse.m2m.qvt.oml/model/trace.ecore");
+		
+		// https://projects.eclipse.org/projects/technology.ogee/developer
+		dataSet.addProject(localPath, "technology.ogee", "http://git.eclipse.org/c/ogee/org.eclipse.ogee.git",
+				"/org.eclipse.ogee.model/model/OData.ecore");
+		
+		// https://projects.eclipse.org/projects/modeling.m2t.acceleo/developer
+		dataSet.addProject(localPath, "modeling.m2t.acceleo", "http://git.eclipse.org/c/acceleo/org.eclipse.acceleo.git",
+				"/plugins/org.eclipse.acceleo.model/model/mtl.ecore",
+				"/plugins/org.eclipse.acceleo.model/model/mtlnonstdlib.ecore",
+				"/plugins/org.eclipse.acceleo.model/model/mtlstdlib.ecore",
+				"/plugins/org.eclipse.acceleo.profiler/model/profiler.ecore",
+				"/plugins/org.eclipse.acceleo.ui.interpreter.completeocl/model/evaluation_result.ecore");
 		
 		// https://projects.eclipse.org/projects/modeling.emft.emf-client/developer
 		dataSet.addProject(localPath, "modeling.emft.emf-client", "http://git.eclipse.org/c/emfclient/org.eclipse.emf.ecp.core.git",
