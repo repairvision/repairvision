9213c609d5a0b6192ed890779af3e7c8dd71e8b3
mohrndorf <manuel.ohrndorf@gmx.net>
2018-08-21 18:11:27 +0000

    resolve provided resource set
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@4120 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.history.model.editor/src/org/sidiff/historymodel/presentation/handlers/ShowEditStepsOnResourceSetHandler.java
+++ plugins/org.sidiff.history.model.editor/src/org/sidiff/historymodel/presentation/handlers/ShowEditStepsOnResourceSetHandler.java
@@ -4,7 +4,9 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -28,16 +30,26 @@ public class ShowEditStepsOnResourceSetHandler extends AbstractHandler {
 				Version beforeIntroduced = EvaluationUtil.getPrecessorRevision(inconsistency.getIntroducedIn());
 				Version introduced = inconsistency.getIntroducedIn();
 				
+				Resource beforeIntroducedResource = new ResourceSetImpl().getResource(getModelURI(beforeIntroduced), true);
+				EcoreUtil.resolveAll(beforeIntroducedResource);
+				Resource introducedResource = new ResourceSetImpl().getResource(getModelURI(introduced), true);
+				EcoreUtil.resolveAll(introducedResource);
+				
 				HistoryModelEditorTools.compare(
-						new ResourceSetImpl().getResource(getModelURI(beforeIntroduced), true).getResourceSet(),
-						new ResourceSetImpl().getResource(getModelURI(introduced), true).getResourceSet());
+						beforeIntroducedResource.getResourceSet(),
+						introducedResource.getResourceSet());
 				
 				Version beforeResolved = EvaluationUtil.getPrecessorRevision(inconsistency.getResolvedIn());
 				Version resolved = inconsistency.getResolvedIn();
 				
+				Resource beforeResolvedResource = new ResourceSetImpl().getResource(getModelURI(beforeResolved), true);
+				EcoreUtil.resolveAll(beforeResolvedResource);
+				Resource resolvedResource = new ResourceSetImpl().getResource(getModelURI(resolved), true);
+				EcoreUtil.resolveAll(resolvedResource);
+				
 				HistoryModelEditorTools.compare(
-						new ResourceSetImpl().getResource(getModelURI(beforeResolved), true).getResourceSet(),
-						new ResourceSetImpl().getResource(getModelURI(resolved), true).getResourceSet());
+						beforeResolvedResource.getResourceSet(),
+						resolvedResource.getResourceSet());
 			}
 		}
 		
