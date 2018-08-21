4cd6ae19f300cc5e9b95f7400f55ee00f05af24b
mohrndorf <manuel.ohrndorf@gmx.net>
2018-08-21 13:54:53 +0000

    reduce evaluation data
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/evaluation@4117 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins.evaluation/org.sidiff.repair.history.generator2/src/org/sidiff/repair/history/generator/ecore/EcoreHistoryReduceApplication.java
+++ plugins.evaluation/org.sidiff.repair.history.generator2/src/org/sidiff/repair/history/generator/ecore/EcoreHistoryReduceApplication.java
@@ -2,6 +2,11 @@ package org.sidiff.repair.history.generator.ecore;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,13 +15,14 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.sidiff.historymodel.History;
 import org.sidiff.historymodel.ValidationError;
 import org.sidiff.historymodel.Version;
-import org.sidiff.repair.history.generator.util.HistoryUtil;
 import org.sidiff.repair.history.evaluation.util.EvaluationUtil;
+import org.sidiff.repair.history.generator.util.HistoryUtil;
 
 public class EcoreHistoryReduceApplication implements IApplication {
 
@@ -40,16 +46,20 @@ public class EcoreHistoryReduceApplication implements IApplication {
 			// Reduce each history to those versions that:
 			// (a) have introduced an inconsistency and its direct predecessor.
 			// (b) have resolved an inconsistency and its direct predecessor.
-			// Remove predecessor/successor reference from inconsistencies.
+			// Remove/Update predecessor/successor reference from inconsistencies.
 			// Remove missing proxy errors.
-			Set<Version> removedVersions = new HashSet<>();
+			// Remove non resolved inconsistencies.
+			Set<Version> removedVersions = new HashSet<>(history.getVersions());
 			
 			for (Version version : history.getVersions()) {
-				removedVersions.add(version);
 				
 				for (ValidationError inconsistency : new ArrayList<>(version.getValidationErrors())) {
-					inconsistency.setPrec(null);
-					inconsistency.setSucc(null);
+					
+					// Remove non resolved inconsistencies:
+					if (!inconsistency.isResolved()) {
+						version.getValidationErrors().remove(inconsistency);
+						continue;
+					}
 					
 					// Remove missing proxy errors:
 					if (inconsistency.getName().equals(EcoreHistorySettings.INCONSISTENCY_UNRESOLVED_PROXY)) {
@@ -58,12 +68,34 @@ public class EcoreHistoryReduceApplication implements IApplication {
 					}
 					
 					// Filter to introduced/resolved inconsistencies:
-					if ((inconsistency.getIntroducedIn() == version) || (inconsistency.getResolvedIn() == version)) {
-						removedVersions.remove(version);
+					Version successorVersion = EvaluationUtil.getSuccessorRevision(version);
+					
+					// Preserve introduced step:
+					if (inconsistency.getIntroducedIn() == version) {
 						removedVersions.remove(EvaluationUtil.getPrecessorRevision(version));
+						removedVersions.remove(version);
+					} 
+					
+					// Preserve resolved step:
+					if (inconsistency.getResolvedIn() == successorVersion) {
+						removedVersions.remove(version);
+						removedVersions.remove(successorVersion);
 					} else {
+						
+						// Remove inconsistency:
+						ValidationError predecessorInconsistency = inconsistency.getPrec();
+						ValidationError successorInconsistency =  inconsistency.getSucc();
+						
+						if (predecessorInconsistency != null) {
+							predecessorInconsistency.setSucc(successorInconsistency);
+						}
+						if (successorInconsistency != null) {
+							successorInconsistency.setPrec(predecessorInconsistency);
+						}
+						
 						version.getValidationErrors().remove(inconsistency);
 					}
+					
 				}
 			}
 			
@@ -72,9 +104,15 @@ public class EcoreHistoryReduceApplication implements IApplication {
 			if (!history.getVersions().isEmpty()) {
 				
 				// Store only included model resource sets:
+				for (Version version : history.getVersions()) {
+					File modelFile = new File(history.eResource().getURI().trimSegments(1).devicePath() + version.getModelURI());
+					copy(modelFile.getParentFile(), toTargetFile(modelFile, true).getParentFile());
+				}
 				
 				// Store history:
-				
+				Resource historyResource = history.eResource();
+				historyResource.setURI(toTargetURI(historyResource.getURI(), true));
+				historyResource.save(Collections.emptyMap());
 			}
 		}
 		
@@ -114,6 +152,16 @@ public class EcoreHistoryReduceApplication implements IApplication {
 		return files;
 	}
 	
+	protected File toTargetFile(File source, boolean mkdirs) {
+		File target =  new File(source.getAbsolutePath().replace(sourceDataSet.getAbsolutePath(), targetDataSet.getAbsolutePath()));
+		
+		if (mkdirs) {
+			target.getParentFile().mkdirs();
+		}
+		
+		return target;
+	}
+	
 	protected URI toTargetURI(URI sourceURI, boolean mkdirs) {
 		URI targetURI =  URI.createURI(sourceURI.toString().replace(sourceDataSetURI.toString(), targetDataSetURI.toString()));
 		
@@ -122,5 +170,33 @@ public class EcoreHistoryReduceApplication implements IApplication {
 		}
 		
 		return targetURI;
+	}
+	
+	public void copy(File sourceLocation, File targetLocation) throws IOException {
+		if (sourceLocation.isDirectory()) {
+			copyDirectory(sourceLocation, targetLocation);
+		} else {
+			copyFile(sourceLocation, targetLocation);
+		}
+	}
+
+	private void copyDirectory(File source, File target) throws IOException {
+		if (!target.exists()) {
+			target.mkdir();
+		}
+
+		for (String f : source.list()) {
+			copy(new File(source, f), new File(target, f));
+		}
+	}
+
+	private void copyFile(File source, File target) throws IOException {
+		try (InputStream in = new FileInputStream(source); OutputStream out = new FileOutputStream(target)) {
+			byte[] buf = new byte[1024];
+			int length;
+			while ((length = in.read(buf)) > 0) {
+				out.write(buf, 0, length);
+			}
+		}
 	}
 }
