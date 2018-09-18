b6ff4fba7f0fda40fb23f542536ef9f7a64e09e1
mohrndorf <manuel.ohrndorf@gmx.net>
2018-09-18 14:02:12 +0000

    resource set matching
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@4215 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.repair.api.peo/src/org/sidiff/repair/api/peo/PEORepairFacade.java
+++ plugins/org.sidiff.repair.api.peo/src/org/sidiff/repair/api/peo/PEORepairFacade.java
@@ -4,6 +4,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.sidiff.history.revision.impl.Revision;
 import org.sidiff.repair.api.IRepairFacade;
 
 /**
@@ -19,13 +20,18 @@ public class PEORepairFacade implements IRepairFacade<PEORepairJob, PEORepairSet
 		// Initialize:
 		ResourceSet differenceRSS = new ResourceSetImpl();
 		Resource modelA = differenceRSS.getResource(uriModelA, true);
-		Resource modelB = differenceRSS.getResource(uriModelB, true);
+		Resource modelB = (uriModelB != null) ? differenceRSS.getResource(uriModelB, true) : null;
 	
 		return new PEORepairCalculationEngine(settings, modelA, modelB).getRepairs();
 	}
 	
 	@Override
 	public PEORepairJob getRepairs(Resource modelA, Resource modelB, PEORepairSettings settings) {
+		
+		if (modelA == null) {
+			modelA = Revision.createEmptyModel(modelB);
+		}
+		
 		return new PEORepairCalculationEngine(settings, modelA, modelB).getRepairs();
 	}
 }
