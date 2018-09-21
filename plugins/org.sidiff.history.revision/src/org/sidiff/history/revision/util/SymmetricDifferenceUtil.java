adb7affd9ee7151ad1699287a13acecb6f5121ed
mohrndorf <manuel.ohrndorf@gmx.net>
2018-09-21 17:17:23 +0000

    refactoring of commons plug-in
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@4255 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.history.revision/src/org/sidiff/history/revision/util/SymmetricDifferenceUtil.java
+++ plugins/org.sidiff.history.revision/src/org/sidiff/history/revision/util/SymmetricDifferenceUtil.java
@@ -1,8 +1,17 @@
 package org.sidiff.history.revision.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EReference;
+import org.sidiff.difference.symmetric.AddObject;
+import org.sidiff.difference.symmetric.AddReference;
+import org.sidiff.difference.symmetric.AttributeValueChange;
+import org.sidiff.difference.symmetric.Change;
+import org.sidiff.difference.symmetric.RemoveObject;
+import org.sidiff.difference.symmetric.RemoveReference;
 import org.sidiff.difference.symmetric.SymmetricPackage;
 
 public class SymmetricDifferenceUtil {
@@ -50,6 +59,86 @@ public class SymmetricDifferenceUtil {
 				|| (type == SYMMETRIC_PACKAGE.getRemoveReference_Type())
 				|| (type == SYMMETRIC_PACKAGE.getAttributeValueChange_Type())) {
 			return true;
+		}
+		
+		return false;
+	}
+	
+	public boolean isChangeEqual(Set<Change> changeSetA, Set<Change> changeSetB) {
+
+		if (changeSetA.size() == changeSetB.size()) {
+			Set<Change> mappedChangesB = new HashSet<>();
+			
+			for (Change changeA : changeSetA) {
+				if (getEqualChange(changeA, changeSetB, mappedChangesB) == null) {
+					return false;
+				}
+			}
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private static Change getEqualChange(Change changeA, Set<Change> changeSetB, Set<Change> mappedChangesB) {
+		
+		for (Change changeB : changeSetB) {
+			if (!mappedChangesB.contains(changeB)) {
+				if (isEqualChange(changeA, changeB)) {
+					mappedChangesB.add(changeB);
+					return changeB;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public static boolean isEqualChange(Change changeA, Change changeB) {
+		
+		if (changeA.getClass() == changeB.getClass()) {
+			
+			if (changeA instanceof RemoveObject) {
+				if (((RemoveObject) changeA).getObj().eClass() == ((RemoveObject) changeB).getObj().eClass()) {
+					return true;
+				}
+			}
+			
+			else if (changeA instanceof RemoveReference) {
+				if (((RemoveReference) changeA).getType().eClass() == ((RemoveReference) changeB).getType().eClass()) {
+					if (((RemoveReference) changeA).getSrc().eClass() == ((RemoveReference) changeB).getSrc().eClass()) {
+						if (((RemoveReference) changeA).getTgt().eClass() == ((RemoveReference) changeB).getTgt().eClass()) {
+							return true;
+						}
+					}
+				}
+			}
+			
+			else if (changeA instanceof AddObject) {
+				if (((AddObject) changeA).getObj().eClass() == ((AddObject) changeB).getObj().eClass()) {
+					return true;
+				}
+			}
+			
+			else if (changeA instanceof AddReference) {
+				if (((AddReference) changeA).getType().eClass() == ((AddReference) changeB).getType().eClass()) {
+					if (((AddReference) changeA).getSrc().eClass() == ((AddReference) changeB).getSrc().eClass()) {
+						if (((AddReference) changeA).getTgt().eClass() == ((AddReference) changeB).getTgt().eClass()) {
+							return true;
+						}
+					}
+				}
+			}
+			
+			else if (changeA instanceof AttributeValueChange) {
+				if (((AttributeValueChange) changeA).getType().eClass() == ((AttributeValueChange) changeB).getType().eClass()) {
+					if (((AttributeValueChange) changeA).getObjA().eClass() == ((AttributeValueChange) changeB).getObjA().eClass()) {
+						if (((AttributeValueChange) changeA).getObjB().eClass() == ((AttributeValueChange) changeB).getObjB().eClass()) {
+							return true;
+						}
+					}
+				}
+			}
 		}
 		
 		return false;
