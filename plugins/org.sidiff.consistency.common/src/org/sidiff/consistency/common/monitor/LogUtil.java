cb8d72c349431fcdc4d5b01625748427c6c21383
mohrndorf <manuel.ohrndorf@gmx.net>
2018-10-06 22:17:37 +0000

    more utils
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@4325 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.consistency.common/src/org/sidiff/consistency/common/monitor/LogUtil.java
+++ plugins/org.sidiff.consistency.common/src/org/sidiff/consistency/common/monitor/LogUtil.java
@@ -1,6 +1,9 @@
 package org.sidiff.consistency.common.monitor;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class LogUtil {
 
@@ -13,6 +16,70 @@ public class LogUtil {
 			LogTime matchingTimerSum = (LogTime) matchingTimerSumAll.get(log.getLastIndex());
 			matchingTimerSum.append(timer);
 		}
+	}
+	
+	public static <T> List<T> merge(String column, Class<? extends T> type, LogTable... tables) {
+		List<T> mergedColumn = new ArrayList<>();
+		
+		for (LogTable table : tables) {
+			mergedColumn.addAll(table.getColumn(column, type));
+		}
+		
+		return mergedColumn;
+	}
+	
+	public static String convertToLatex(LogTable log) {
+		StringBuilder table = new StringBuilder();
+		List<String> header = new ArrayList<>(log.getColumns());
+		
+		// calculate layout:
+		int[] minColumnSize = new int[header.size()];
+		
+		for (int i = 0; i < header.size(); i++) {
+			String column = header.get(i);
+			
+			if (column.length() > minColumnSize[i]) {
+				minColumnSize[i] = column.length();
+			}
+			
+			for (Object value : log.getColumn(column)) {
+				String stringValue = log.valueToString(value);
+				
+				if (stringValue.length() > minColumnSize[i]) {
+					minColumnSize[i] = stringValue.length();
+				}
+			}
+		}
+		
+		// print table:
+		for (int i = 0; i < header.size(); i++) {
+			String column = header.get(i);
+			
+			table.append(column);
+			table.append(StringUtils.repeat(" ", minColumnSize[i] - column.length()));
+			
+			if (i < (header.size() - 1)) {
+				table.append(" & ");
+			}
+		}
+		table.append(" \\\\\n");
+		
+		for (int i = 0; i < log.getLastIndex(); i++) {
+			for (int j = 0; j < header.size(); j++) {
+				String column = header.get(j);
+				String value = log.valueToString(log.getColumn(column).get(i));
+				
+				table.append(value);
+				table.append(StringUtils.repeat(" ", minColumnSize[j] - value.length()));
+				
+				if (j < (header.size() - 1)) {
+					table.append(" & ");
+				}
+			}
+			table.append(" \\\\\n");
+		}
+		
+		return table.toString();
 	}
 	
 	public static Object division(int a, int b) {
