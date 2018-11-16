789e851ee0aed7962d7ba4cbcca137f4bc0ec6e7
mohrndorf <manuel.ohrndorf@gmx.net>
2018-11-16 19:24:53 +0000

    create data structure to debug node pattern domains
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/plugins@4433 db72bd86-da5d-42d1-ad72-61f8f7c676a0

--- plugins/org.sidiff.editrule.recognition/src/org/sidiff/editrule/recognition/util/debug/DebugUtil.java
+++ plugins/org.sidiff.editrule.recognition/src/org/sidiff/editrule/recognition/util/debug/DebugUtil.java
@@ -1,12 +1,18 @@
 package org.sidiff.editrule.recognition.util.debug;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.henshin.model.Rule;
 import org.sidiff.consistency.common.monitor.LogTime;
 import org.sidiff.editrule.recognition.IMatching;
 import org.sidiff.editrule.recognition.pattern.RecognitionPattern;
+import org.sidiff.editrule.recognition.pattern.domain.Domain;
 import org.sidiff.editrule.recognition.pattern.graph.ActionNode;
+import org.sidiff.graphpattern.EdgePattern;
 import org.sidiff.graphpattern.NodePattern;
+import org.sidiff.common.emf.provider.ItemProviderUtil;
 
 public class DebugUtil {
 
@@ -27,6 +33,59 @@ public class DebugUtil {
 	//
 	// }
 	// }
+	
+	public final class VariableWithDomain {
+		
+		private NodePattern node;
+		
+		private List<EObject> domain;
+		
+		public VariableWithDomain(NodePattern node, List<EObject> domain) {
+			this.node = node;
+			this.domain = domain;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuffer print = new StringBuffer();
+			
+			print.append("Node: " + node.getName());
+			print.append(", Type: " + node.getType().getName());
+			
+			print.append(", Outgoings: ");
+			
+			for (EdgePattern outgoing : node.getOutgoings()) {
+				if (outgoing != node.getOutgoings().get(0)) {
+					print.append(", ");
+				}
+				print.append(ItemProviderUtil.getTextByObject(outgoing));
+			}
+			
+			print.append(":\n");
+			
+			for (int i = 0; i < domain.size(); i++) {
+				print.append("  [" + i + "] " + ItemProviderUtil.getTextByObject(domain.get(i)) + "\n");
+			}
+			
+			return print.toString();
+		}
+	}
+	
+	public static List<VariableWithDomain> getDomains(List<NodePattern> nodes) {
+		List<VariableWithDomain> domains = new ArrayList<>();
+		
+		for (NodePattern node : nodes) {
+			domains.add(new DebugUtil().new VariableWithDomain(node, getDomain(node)));
+		}
+		
+		return domains;
+	}
+	
+	public static List<EObject> getDomain(NodePattern node) {
+		List<EObject> domain = new ArrayList<>();
+		Domain.get(node).iterator().forEachRemaining(domain::add);
+		return domain;
+	}
 
 	public static void printEditRule(Rule editRule) {
 		if (EVALUATION) {
