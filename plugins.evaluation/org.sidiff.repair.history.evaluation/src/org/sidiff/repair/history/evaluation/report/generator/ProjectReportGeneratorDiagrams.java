df78196d9e27e23779d873b4e2972feede4f56b3
mohrndorf <manuel.ohrndorf@gmx.net>
2018-11-30 16:38:51 +0000

    tables for diagrams
    git-svn-id: http://svn.informatik.uni-siegen.de/DA.mohrndorf/trunk/projects/consistency/evaluation@4474 db72bd86-da5d-42d1-ad72-61f8f7c676a0

---
+++ plugins.evaluation/org.sidiff.repair.history.evaluation/src/org/sidiff/repair/history/evaluation/report/generator/ProjectReportGeneratorDiagrams.java
@@ -0,0 +1,71 @@
+package org.sidiff.repair.history.evaluation.report.generator;
+
+import static org.sidiff.consistency.common.monitor.LogUtil.merge;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.sidiff.consistency.common.monitor.LogTable;
+import org.sidiff.repair.history.evaluation.report.HistoryLog;
+import org.sidiff.repair.history.evaluation.report.InconsistenciesLog;
+
+public class ProjectReportGeneratorDiagrams {
+	
+	private static final boolean ENABLED = false;
+	
+	private static final String OUTPUT_FOLDER = "C:\\workspaces\\sidiff-build\\org.sidiff.repair.history.evaluation\\results\\";
+
+	public ProjectReportGeneratorDiagrams() throws IOException, IllegalArgumentException, IllegalAccessException  {
+		if (!ENABLED) return;
+		
+		Map<String, List<EvaluationData>> evaluationDataPerProjects = ReportGenerator.getEvaluationsPerProject();
+
+		for (Entry<String, List<EvaluationData>> evaluationDataPerProject : evaluationDataPerProjects.entrySet()) {
+			List<LogTable> inconsistenciesLogs = evaluationDataPerProject.getValue().stream().map(evaluation -> evaluation.inconsistenciesLog).collect(Collectors.toList());
+			List<LogTable> historyLogs = evaluationDataPerProject.getValue().stream().map(evaluation -> evaluation.historyLog).collect(Collectors.toList());
+			
+			LogTable rq3_repairs = merge(inconsistenciesLogs,
+					InconsistenciesLog.COL_INCONSISTENCY,
+					InconsistenciesLog.COL_CONTEXT_ELEMENT,
+					InconsistenciesLog.COL_COMPLEMENTS,
+					InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_REPAIRS,
+					InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_UNDOS,
+					InconsistenciesLog.COL_RANKING_OF_BEST_HOR);
+			
+			rq3_repairs.toCSV(OUTPUT_FOLDER + evaluationDataPerProject.getKey() + "_rq3_repairs.csv");
+			
+			LogTable rq3_repairs_hor = merge(inconsistenciesLogs,
+					InconsistenciesLog.COL_INCONSISTENCY,
+					InconsistenciesLog.COL_CONTEXT_ELEMENT,
+					InconsistenciesLog.COL_COMPLEMENTS,
+					InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_REPAIRS,
+					InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_UNDOS,
+					InconsistenciesLog.COL_RANKING_OF_BEST_HOR);
+			
+			rq3_repairs_hor.filterRows(row -> 
+				!row.get(InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_REPAIRS, Boolean.class) &&
+				!row.get(InconsistenciesLog.COL_HISTORICALLY_OBSERVABLE_UNDOS, Boolean.class));
+			
+			rq3_repairs_hor.toCSV(OUTPUT_FOLDER + evaluationDataPerProject.getKey() + "_rq3_repairs_hor.csv");
+			
+			
+			LogTable rq4_modelelements = merge(historyLogs,
+					HistoryLog.COL_HISTORY,
+					HistoryLog.COL_AVG_ELEMENTS);
+			
+			rq4_modelelements.toCSV(OUTPUT_FOLDER + evaluationDataPerProject.getKey() + "_rq4_modelelements.csv");
+			
+			LogTable rq4_repairs = merge(inconsistenciesLogs,
+					InconsistenciesLog.COL_INCONSISTENCY,
+					InconsistenciesLog.COL_CONTEXT_ELEMENT,
+					InconsistenciesLog.COL_TIME_LOAD_CALCULATE_REVISION,
+					InconsistenciesLog.COL_TIME_RECOGNITION,
+					InconsistenciesLog.COL_TIME_COMPLEMENT_MATCHING);
+			
+			rq4_repairs.toCSV(OUTPUT_FOLDER + evaluationDataPerProject.getKey() + "_rq4_runtime.csv");
+		}
+	}
+}
--- plugins.evaluation/org.sidiff.repair.history.evaluation/src/org/sidiff/repair/history/evaluation/report/generator/ReportGenerator.java
+++ plugins.evaluation/org.sidiff.repair.history.evaluation/src/org/sidiff/repair/history/evaluation/report/generator/ReportGenerator.java
@@ -41,6 +41,7 @@ public class ReportGenerator implements IApplication {
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
 		new ProjectReportGenerator();
+		new ProjectReportGeneratorDiagrams();
 		new InconsistencyReportGenerator();
 		new EditRuleReportGenerator();
 		
