4f991d0f068a68dcea1e4392f9a547eafe4b21fa
mohrndorf <manuel.ohrndorf@gmx.net>
2019-06-22 15:21:57 +0200

    doc

--- plugins/org.sidiff.csp.solver/src/org/sidiff/csp/solver/ISolutions.java
+++ plugins/org.sidiff.csp.solver/src/org/sidiff/csp/solver/ISolutions.java
@@ -6,5 +6,11 @@ public interface ISolutions<R, D, S extends ISolution<R, D>> {
 	
 	void reset();
 	
+	/**
+	 * Solution factory.
+	 * 
+	 * @param size The size of the solution.
+	 * @return A new solution.
+	 */
 	S createSolution(int size);
 }
