f49743a70a31a7815d90f19bdc045da9fbecbd34
mohrndorf <manuel.ohrndorf@gmx.net>
2019-06-26 15:52:55 +0200

    calculate choices for template placeholder based on alternatives

--- plugins.research/org.sidiff.completion.ui/src/org/sidiff/completion/ui/model/ModelCompletionProposalCluster.java
+++ plugins.research/org.sidiff.completion.ui/src/org/sidiff/completion/ui/model/ModelCompletionProposalCluster.java
@@ -7,16 +7,19 @@ import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.TEMPLAT
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.TEMPLATE_PARAMETER_SEPARATOR;
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.TEMPLATE_PRESENCE_SEPARATOR;
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.TEMPLATE_PRESENCE_SEPARATOR_TRIM;
-import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.getPlainTemplate;
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.generateDecompositionSequenceFromHierarchicals;
+import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.getPlainTemplate;
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.getSubGraphChanges;
 import static org.sidiff.completion.ui.model.ModelCompletionProposalUtil.getSubGraphContext;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.function.Function;
 
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.henshin.model.Attribute;
 import org.eclipse.emf.henshin.model.Edge;
 import org.eclipse.emf.henshin.model.HenshinPackage;
@@ -27,11 +30,13 @@ import org.sidiff.completion.ui.codebricks.Brick;
 import org.sidiff.completion.ui.codebricks.Codebrick;
 import org.sidiff.completion.ui.codebricks.Codebricks;
 import org.sidiff.completion.ui.codebricks.CodebricksFactory;
+import org.sidiff.completion.ui.codebricks.ComposedBrick;
 import org.sidiff.completion.ui.codebricks.LineBreakBrick;
 import org.sidiff.completion.ui.codebricks.ObjectPlaceholderBrick;
 import org.sidiff.completion.ui.codebricks.TemplatePlaceholderBrick;
 import org.sidiff.completion.ui.codebricks.TextBrick;
 import org.sidiff.completion.ui.codebricks.ValuePlaceholderBrick;
+import org.sidiff.completion.ui.codebricks.ViewableBrick;
 import org.sidiff.completion.ui.codebricks.editor.CodebricksEditor;
 import org.sidiff.completion.ui.list.ICompletionPreview;
 import org.sidiff.completion.ui.list.ICompletionProposal;
@@ -148,10 +153,10 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 			String searchedTemplateA = matchTemplatesAItr.next();
 			
 			while(inTemplatesBItr.hasNext()) {
-				String nextHistoricTemplate = inTemplatesBItr.next();
+				String nextTemplateB = inTemplatesBItr.next();
 				
 				// Match against largest main template:
-				if (searchedTemplateA.equals(nextHistoricTemplate)) {
+				if (searchedTemplateA.equals(nextTemplateB)) {
 					if (matchTemplatesAItr.hasNext()) {
 						searchedTemplateA = matchTemplatesAItr.next();
 					} else {
@@ -161,6 +166,62 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 				}
 			}
 		}
+		return false;
+	}
+	
+	private boolean matchAlternativeToTemplates(
+			LinkedHashMap<ComposedBrick, String> matchAlternative,
+			LinkedHashMap<ComposedBrick, String> inTemplate) {
+		
+		// TODO: SEE NOTE*1
+		
+		if (!inTemplate.isEmpty() && !matchAlternative.isEmpty() && (matchAlternative.size() <= inTemplate.size())) {
+			Iterator<Entry<ComposedBrick, String>> matchAlternativeItr = matchAlternative.entrySet().iterator();
+			Iterator<Entry<ComposedBrick, String>> inTemplatesItr = inTemplate.entrySet().iterator();
+			
+			Entry<ComposedBrick, String> searchedAlternative = matchAlternativeItr.next();
+			
+			while(inTemplatesItr.hasNext()) {
+				Entry<ComposedBrick, String> nextTemplate = inTemplatesItr.next();
+				
+				// Match by template text:
+				String template = DecomposingEditRulesUtil.getFirstLevelTemplate(nextTemplate.getValue());
+				String alternativeTemplate = DecomposingEditRulesUtil.getFirstLevelTemplate(searchedAlternative.getValue());
+				
+				if (template.equals(alternativeTemplate)) {
+
+					// Template has placeholder? 
+					if (DecomposingEditRulesUtil.containsPlaceholder(template)) {
+						TemplatePlaceholderBrick placeholder = null;
+						
+						if (nextTemplate.getKey() instanceof TemplatePlaceholderBrick) {
+							placeholder = (TemplatePlaceholderBrick) nextTemplate.getKey();
+						} else if (nextTemplate.getKey() instanceof ComposedBrick) {
+							for (ViewableBrick brick : ((ComposedBrick) nextTemplate.getKey()).getBricks()) {
+								if (brick instanceof TemplatePlaceholderBrick) {
+									placeholder = (TemplatePlaceholderBrick) brick;
+									break;
+								}
+							}
+						}
+						
+						// Add to choices:
+						if (placeholder != null) {
+							placeholder.getChoices().add(searchedAlternative.getKey());
+						}
+					}
+					
+					
+					if (matchAlternativeItr.hasNext()) {
+						searchedAlternative = matchAlternativeItr.next();
+					} else {
+						// All alternatives matched!
+						return true;
+					}
+				}
+			}
+		}
+		
 		return false;
 	}
 
@@ -223,9 +284,9 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 		codebricks.setTemplate(template);
 		
 		// Historic:
-		createEditRuleSequenceCodebrick(
-				template, intersectionHistoricTemplates,
-				supersetProposal.getHistoricTemplates(), supersetProposal::getHistoricByTemplate);
+		LinkedHashMap<ComposedBrick, String> historicEditRules = createEditRuleSequenceCodebrick(
+				template, intersectionHistoricTemplates, supersetProposal.getHistoricTemplates(), 
+				supersetProposal::getHistoricByTemplate);
 		
 		// Presence:
 		LineBreakBrick lineBreakBeforeHistoricalSeparator = CodebricksFactory.eINSTANCE.createLineBreakBrick();
@@ -236,21 +297,66 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 		template.getBricks().add(historicalComplementSeparatorBrick);
 		
 		// Complement:
-		createEditRuleSequenceCodebrick(
-				template, intersectionComplementTemplates,
-				supersetProposal.getComplementFirstLevel(), supersetProposal::getComplementByFirstLevel);
+		LinkedHashMap<ComposedBrick, String> complementEditRules = createEditRuleSequenceCodebrick(
+				template, intersectionComplementTemplates, supersetProposal.getComplementFirstLevel(), 
+				supersetProposal::getComplementByFirstLevel);
+		
+		// Generate alternatives:
+		createAlternatives(codebricks, historicEditRules, complementEditRules);
 		
 		return codebricks;
 	}
+	
+	private void createAlternatives(Codebricks codebricks,
+			LinkedHashMap<ComposedBrick, String> historicTemplate,
+			LinkedHashMap<ComposedBrick, String> complementTemplate) {
+		
+		for (ModelCompletionProposal proposal : proposalCluster) {
+			DecompositionTemplates decomposition = proposal.getDecomposition();
+			
+			// Create alternative:
+			Codebrick template = CodebricksFactory.eINSTANCE.createCodebrick();
+			codebricks.getAlternatives().add(template);
+			
+			// Historic:
+			LinkedHashMap<ComposedBrick, String> historicAlternative = createEditRuleSequenceCodebrick(
+					template, null, decomposition.getHistoricTemplates(), 
+					decomposition::getHistoricByTemplate);
+			
+			// Presence:
+			LineBreakBrick lineBreakBeforeHistoricalSeparator = CodebricksFactory.eINSTANCE.createLineBreakBrick();
+			template.getBricks().add(lineBreakBeforeHistoricalSeparator);
+			
+			TextBrick historicalComplementSeparatorBrick = CodebricksFactory.eINSTANCE.createTextBrick();
+			historicalComplementSeparatorBrick.setText(TEMPLATE_PRESENCE_SEPARATOR_TRIM + " ");
+			template.getBricks().add(historicalComplementSeparatorBrick);
+			
+			// Complement:
+			LinkedHashMap<ComposedBrick, String> complementAlternative = createEditRuleSequenceCodebrick(
+					template, null, decomposition.getComplementTemplates(), 
+					decomposition::getComplementByTemplate);
+			
+			// Map alternatives to template, i.e, calculate choices:
+			// NOTE: There should be no choices for the historic template if it is common to all alternatives.
+			matchAlternativeToTemplates(historicAlternative, historicTemplate); 
+			matchAlternativeToTemplates(complementAlternative, complementTemplate);
+		}
+	}
 
-	private void createEditRuleSequenceCodebrick(
+	/**
+	 * @return Bricks of the sub edit rules. 
+	 */
+	private LinkedHashMap<ComposedBrick, String> createEditRuleSequenceCodebrick(
 			Codebrick codebrick, List<String> intersectionTemplates,
 			List<String> templates, Function<String, SubGraph> templateToRule) {
 		
+		LinkedHashMap<ComposedBrick, String> subEditRules = new LinkedHashMap<>();
+		
 		for (String template : templates) {
+			ComposedBrick subEditRule = CodebricksFactory.eINSTANCE.createComposedBrick();
 			
 			// Common?
-			if (intersectionTemplates.contains(DecomposingEditRulesUtil.getFirstLevelTemplate(template))) {
+			if ((intersectionTemplates == null) || intersectionTemplates.contains(DecomposingEditRulesUtil.getFirstLevelTemplate(template))) {
 				
 				// Sub edit rule template?
 				if (DecomposingEditRulesUtil.containsPlaceholder(template)) {
@@ -259,26 +365,26 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 					TemplatePlaceholderBrick complementSubEditRuleBrick = CodebricksFactory.eINSTANCE.createTemplatePlaceholderBrick();
 					complementSubEditRuleBrick.setPlaceholder(plainTemplate);
 					complementSubEditRuleBrick.setMandatory(true);
-					codebrick.getBricks().add(complementSubEditRuleBrick);
+					subEditRule.getBricks().add(complementSubEditRuleBrick);
 					
 					// Not show parameters for template:
 					// SEE NOTE*1 
 					TextBrick ruleParameterOpeningBracesBrick = CodebricksFactory.eINSTANCE.createTextBrick();
 					ruleParameterOpeningBracesBrick.setText("(");
-					codebrick.getBricks().add(ruleParameterOpeningBracesBrick);
+					subEditRule.getBricks().add(ruleParameterOpeningBracesBrick);
 					
 					TextBrick ruleParameterClosingBracesBrick = CodebricksFactory.eINSTANCE.createTextBrick();
 					ruleParameterClosingBracesBrick.setText(")");
-					codebrick.getBricks().add(ruleParameterClosingBracesBrick);
+					subEditRule.getBricks().add(ruleParameterClosingBracesBrick);		
 					
 				} else {
 					String plainFirstLevelTemplate = getPlainTemplate(template);
 					
 					TextBrick complementSubEditRuleBrick = CodebricksFactory.eINSTANCE.createTextBrick();
 					complementSubEditRuleBrick.setText(plainFirstLevelTemplate);
-					codebrick.getBricks().add(complementSubEditRuleBrick);
+					subEditRule.getBricks().add(complementSubEditRuleBrick);
 					
-					createTemplateParameters(codebrick, templateToRule.apply(template));
+					createTemplateParameters(subEditRule, templateToRule.apply(template));
 				}
 				
 			} else {
@@ -287,8 +393,12 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 				TemplatePlaceholderBrick complementSubEditRuleBrick = CodebricksFactory.eINSTANCE.createTemplatePlaceholderBrick();
 				complementSubEditRuleBrick.setPlaceholder(TEMPLATE_OPTIONAL_PLACEHOLDER);
 				complementSubEditRuleBrick.setMandatory(false);
-				codebrick.getBricks().add(complementSubEditRuleBrick);
+				subEditRule.getBricks().add(complementSubEditRuleBrick);
 			}
+			
+			// Add sub edit rule to template:
+			codebrick.getBricks().add(subEditRule);
+			subEditRules.put(subEditRule, template); // NOTE: the rule template string is not necessarily unique.
 			
 			// Not last rule?
 			if (template != templates.get(templates.size() -1)) {
@@ -300,9 +410,11 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 				codebrick.getBricks().add(ruleSeparatorBrick);
 			}
 		}
+		
+		return subEditRules;
 	}
 
-	private void createTemplateParameters(Codebrick template, SubGraph subEditRule) {
+	private void createTemplateParameters(ComposedBrick template, SubGraph subEditRule) {
 		
 		// NOTE: Show (all) IN parameters of subEditRule (hide OUT parameters):
 		
@@ -312,15 +424,17 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 		template.getBricks().add(ruleParameterOpeningBracesBrick);
 		
 		// Create parameters for sub edit rule:
-		Brick contextParameterSeparator = createParametersForSubGraph(template, getSubGraphContext(subEditRule));
-		Brick changesParameterSeparator = createParametersForSubGraph(template, getSubGraphChanges(subEditRule));
+		List<ViewableBrick> parameterList = new ArrayList<>();
+		Brick contextParameterSeparator = createParametersForSubGraph(parameterList, getSubGraphContext(subEditRule));
+		Brick changesParameterSeparator = createParametersForSubGraph(parameterList, getSubGraphChanges(subEditRule));
+		template.getBricks().addAll(parameterList);
 		
 		// Remove last ',':
 		if ((changesParameterSeparator == null) && (contextParameterSeparator != null)) {
-			template.getBricks().remove(contextParameterSeparator);
+			EcoreUtil.remove(contextParameterSeparator);
 		}
 		if (changesParameterSeparator != null) {
-			template.getBricks().remove(changesParameterSeparator);
+			EcoreUtil.remove(changesParameterSeparator);
 		}
 		
 		// add ')':
@@ -329,7 +443,7 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 		template.getBricks().add(ruleParameterClosingBracesBrick);
 	}
 
-	private Brick createParametersForSubGraph(Codebrick template, List<GraphElementExtension> subGraph) {
+	private Brick createParametersForSubGraph(List<ViewableBrick> parameterBricks, List<GraphElementExtension> subGraph) {
 		TextBrick parameterSeparator = null;
 		
 		for (GraphElementExtension graphElement : subGraph) {
@@ -337,20 +451,33 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 				Node eoNode = (Node) graphElement;
 				
 				if (HenshinRuleAnalysisUtilEx.isDeletionNode(eoNode) || hasEdgeChanges(eoNode)) {
-					generateObjectParameter(template, eoNode.getName());
+					String name = eoNode.getName();
 					
-					parameterSeparator = CodebricksFactory.eINSTANCE.createTextBrick();
-					parameterSeparator.setText(TEMPLATE_PARAMETER_SEPARATOR);
-					template.getBricks().add(parameterSeparator);
+					// Check if parameter already exists:
+					if (!parameterBricks.stream().anyMatch(b -> b.getText().startsWith(name + TEMPLATE_PARAMETER_NAME_VALUE_SEPARATOR))) {
+						ComposedBrick newParameter = generateObjectParameter(name);
+						
+						parameterSeparator = CodebricksFactory.eINSTANCE.createTextBrick();
+						parameterSeparator.setText(TEMPLATE_PARAMETER_SEPARATOR);
+						newParameter.getBricks().add(parameterSeparator);
+						
+						parameterBricks.add(newParameter);
+					}
 				}
 				
 				for (Attribute attribute : eoNode.getAttributes()) {
 					for (String variable : JavaSciptParser.getVariables(attribute.getValue())) {
-						generateValueParameter(template, variable);
 						
-						parameterSeparator = CodebricksFactory.eINSTANCE.createTextBrick();
-						parameterSeparator.setText(TEMPLATE_PARAMETER_SEPARATOR);
-						template.getBricks().add(parameterSeparator);
+						// Check if parameter already exists:
+						if (!parameterBricks.stream().anyMatch(b -> b.getText().startsWith(variable + TEMPLATE_PARAMETER_NAME_VALUE_SEPARATOR))) {
+							ComposedBrick newParameter = generateValueParameter(variable);
+							
+							parameterSeparator = CodebricksFactory.eINSTANCE.createTextBrick();
+							parameterSeparator.setText(TEMPLATE_PARAMETER_SEPARATOR);
+							newParameter.getBricks().add(parameterSeparator);
+							
+							parameterBricks.add(newParameter);
+						}
 					}
 				}
 			}
@@ -359,26 +486,34 @@ public class ModelCompletionProposalCluster implements ICompletionProposal {
 		return parameterSeparator;
 	}
 	
-	private void generateValueParameter(Codebrick template, String name) {
+	private ComposedBrick generateValueParameter(String name) {
 		TextBrick parameterName = CodebricksFactory.eINSTANCE.createTextBrick();
 		parameterName.setText(name + TEMPLATE_PARAMETER_NAME_VALUE_SEPARATOR);
-		template.getBricks().add(parameterName);
 		
 		ValuePlaceholderBrick parameter = CodebricksFactory.eINSTANCE.createValuePlaceholderBrick();
 		parameter.setPlaceholder(TEMPLATE_MANDATORY_PLACEHOLDER);
 		parameter.setMandatory(true);
-		template.getBricks().add(parameter);
+		
+		ComposedBrick parameterAssignment = CodebricksFactory.eINSTANCE.createComposedBrick();
+		parameterAssignment.getBricks().add(parameterName);
+		parameterAssignment.getBricks().add(parameter);
+		
+		return parameterAssignment;
 	}
 	
-	private void generateObjectParameter(Codebrick template, String name) {
+	private ComposedBrick generateObjectParameter(String name) {
 		TextBrick parameterName = CodebricksFactory.eINSTANCE.createTextBrick();
 		parameterName.setText(name + TEMPLATE_PARAMETER_NAME_VALUE_SEPARATOR);
-		template.getBricks().add(parameterName);
 		
 		ObjectPlaceholderBrick parameter = CodebricksFactory.eINSTANCE.createObjectPlaceholderBrick();
 		parameter.setPlaceholder(TEMPLATE_MANDATORY_PLACEHOLDER);
 		parameter.setMandatory(true);
-		template.getBricks().add(parameter);
+		
+		ComposedBrick parameterAssignment = CodebricksFactory.eINSTANCE.createComposedBrick();
+		parameterAssignment.getBricks().add(parameterName);
+		parameterAssignment.getBricks().add(parameter);
+		
+		return parameterAssignment;
 	}
 	
 	private boolean hasEdgeChanges(Node node) {
