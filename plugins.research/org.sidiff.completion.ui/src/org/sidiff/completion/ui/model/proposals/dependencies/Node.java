cef6d59f4daf3215194d48d06c00ddb9f02e5ab1
gmeidanipour <gmeidanipour@gmmobil.informatik.uni-siegen.de>
2019-07-08 12:27:46 +0200

    dependency ordering

---
+++ plugins.research/org.sidiff.completion.ui/src/org/sidiff/completion/ui/model/proposals/dependencies/Node.java
@@ -0,0 +1,39 @@
+package org.sidiff.completion.ui.model.proposals.dependencies;
+
+public class Node {
+
+    private	String name;
+
+
+
+	public Node(String name) {
+		super();
+		this.name = name;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Node [name=" + name + "]";
+	}
+
+
+
+    
+    
+    
+	
+}
