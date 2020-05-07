cc42c2f6f555922c244cf69666b929d86d42a46d
manue <manue@ThinkPadMO.mshome.net>
2020-05-07 23:19:21 +0200

    edit rule and constraint configuration
    - configuration of rulebase and constraint library on preference page
    - load edit rules from workspace (development time)

---
+++ plugins/org.sidiff.revision.editrules.project.builder.development/src/org/sidiff/revision/editrules/project/builder/development/registry/WorkspaceRulebaseExtension.java
@@ -0,0 +1,21 @@
+package org.sidiff.revision.editrules.project.builder.development.registry;
+
+import org.sidiff.revision.editrules.project.registry.RulebaseExtension;
+
+public class WorkspaceRulebaseExtension extends RulebaseExtension {
+
+	private RulebaseExtension runtime;
+	
+	public WorkspaceRulebaseExtension(String projectName, String runtimeName, String workspaceName, String documentType, String folder) {
+		super(projectName, workspaceName, documentType, folder);
+		this.runtime = new RulebaseExtension(projectName, runtimeName, documentType, folder);
+	}
+
+	public String getRuntimeName() {
+		return runtime.getName();
+	}
+	
+	public boolean equalsRuntime(RulebaseExtension runtime) {
+		return this.runtime.equals(runtime);
+	}
+}
